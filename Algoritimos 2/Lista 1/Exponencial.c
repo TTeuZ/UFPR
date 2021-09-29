0e75d268548be98d819f86c592b2e28e489303dc
TTeuZ <paulomateus.luza@hotmail.com>
2021-09-29 17:34:50 -0300

    [add] Exercicios da Lista 1 de Alg 2

--- Algoritimos 2/Lista 1/Exponencial.c
+++ Algoritimos 2/Lista 1/Exponencial.c
@@ -12,3 +12,35 @@ void main(int argc, char *argv[]) {
 }
 
 // ./exp 2 4 --> 16
+
+/* Caso devolvendo 1 na base
+seja m(n) o numero de multiplicações efetuadas pelo algoritimo.
+n = y;
+
+m(n) = { 1, se n = 0; 1 + m(n-1), se n > 0 }
+
+m(n) = 1 + m(n-1) --> 1 + 1 + m(n-2) --> 1 + 1 + 1 + m(n-3) --> i + m(n-i)
+
+o algoritimo é finalizado em n - i = 0 --> n = i
+
+n + m(n-n) --> n + m(o) --> n + 1
+*/
+
+/* Caso devolvendo 0 na base
+seja m(n) o numero de multiplicações efetuadas pelo algoritimo.
+n = y;
+
+m(n) = { 0, se n = 0; 1 + m(n-1), se n > 0 }
+
+m(n) = 1 + m(n-1) --> 1 + 1 + m(n-2) --> 1 + 1 + 1 + m(n-3) --> i + m(n-i)
+
+o algoritimo é finalizado em n - i = 0 --> n = i
+
+n + m(n-n) --> n + m(o) --> n + 0 --> n
+*/
+
+/* 
+Se eu devolver 0 na base o negócio ta certo, mas não sei se pode isso.
+Mas se for seguir a logica de se no caso base eu não entro em nenhuma 
+multiplicação e por isso retorna 0 então da boa.
+*/
--- Algoritimos 2/Lista 1/Fatorial.c
+++ Algoritimos 2/Lista 1/Fatorial.c
@@ -8,3 +8,29 @@ void main (int argc, char **argv) {
     int x = 10;
     printf("%d \n", fatorial(x));
 }
+
+/*Caso devolvendo 1 na base
+Seja m(n) o numero de multiplicaçoes realizadas pelo algoritimo.
+n = x
+
+m(n) = { 1, se n = 0; 1 + m(n-1), se n > 0 }
+
+m(n) = 1 + m(n-1) --> 1 + 1 + m(n-2) --> 1 + 1 + 1 + m(n-3) --> i + m(n-i)
+
+o algoritimo é finalizado em n - i = 0 --> n = i
+
+n + m(n-n) --> n + m(o) --> n + 1
+*/
+
+/*Caso devolvendo 0 na base
+Seja m(n) o numero de multiplicaçoes realizadas pelo algoritimo.
+n = x
+
+m(n) = { 0, se n = 0; 1 + m(n-1), se n > 0 }
+
+m(n) = 1 + m(n-1) --> 1 + 1 + m(n-2) --> 1 + 1 + 1 + m(n-3) --> i + m(n-i)
+
+o algoritimo é finalizado em n - i = 0 --> n = i
+
+n + m(n-n) --> n + m(o) --> n + 0 --> n
+*/
