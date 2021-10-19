13cfbb7d8c5fe2c153811d72e36e2fe41bbe2fa7
TTeuZ <paulomateus.luza@hotmail.com>
2021-10-19 01:20:29 -0300

    [add] sort por quicksort

--- Algoritimos 2/Trabalho 1/src/particiona.c
+++ Algoritimos 2/Trabalho 1/src/particiona.c
@@ -1,14 +1,18 @@
 #include "biblioteca.h"
 
 /* -------------------------------------------------------------------------- */
-/* modifica v e devolve um índice m de forma que */
+/* modifica v e devolve um ï¿½ndice m de forma que */
 /* a-1 <= m <= b */
 /* v[i] <= x, para todo a <= i <=m */
 /* x < v[i], para todo m < i <=b */
 
 int particiona(int v[], int a, int b, int x) {
-
-  int m;
-
+  int m = a-1;
+  for (int i = a; i <= b; i++) {
+    if (compara(v[i], x) <= 0) {
+      m++;
+      troca(v, m, i);
+    }
+  }
   return m;
 }
--- Algoritimos 2/Trabalho 1/src/quicksort.c
+++ Algoritimos 2/Trabalho 1/src/quicksort.c
@@ -4,6 +4,9 @@
 /* ordena v[a..b] usando o algoritmo QuickSort e devolve v */
 
 int *quicksort(int v[], int a, int b) {
-  
+  if (a >= b) return v;
+  int m = particiona(v, a, b, v[b]);
+  quicksort(v, a, m-1);
+  quicksort(v, m+1, b);
   return v;
 }
