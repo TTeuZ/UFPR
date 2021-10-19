c89ed125a7872e9dd07548dcf99a943375ea12db
TTeuZ <paulomateus.luza@hotmail.com>
2021-10-19 01:19:48 -0300

    [add] sort por inserction

--- Algoritimos 2/Trabalho 1/src/insercao.c
+++ Algoritimos 2/Trabalho 1/src/insercao.c
@@ -1,9 +1,34 @@
 #include "biblioteca.h"
 
 /* -------------------------------------------------------------------------- */
-/* ordena v[a..b] pelo método da seleção e devolve v */
+/* Encontra a posiÃ§Ã£o do elemento, indicando aonde ele deveria estar posicionado
+no array ordenado. */
+
+int find(int x,int v[], int a, int b) {
+    if (a > b) return a-1;
+    if (compara(x, v[b]) >= 0) return b;
+    return find(x, v, a, b-1);
+}
+
+/* -------------------------------------------------------------------------- */
+/* Insere o elemento na posiÃ§Ã£o que ele deveria estar no array */
+
+int *insert(int v[], int a, int b) {
+    int p = find(v[b], v, a, b-1);
+    int i = b;
+    while (i > p + 1) {
+      troca(v, i, i-1);
+      i--;
+    }
+    return v;
+}
+
+/* -------------------------------------------------------------------------- */
+/* ordena v[a..b] pelo mï¿½todo da seleï¿½ï¿½o e devolve v */
 
 int *insercao(int v[], unsigned int a, unsigned int b) {
-
+  if (a >= b) return v;
+  insercao(v, a, b-1);
+  insert(v, a, b);
   return v;
 }
