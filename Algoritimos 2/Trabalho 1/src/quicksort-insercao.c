bcf4639409ef70c77a1c94b99964385d541be505
TTeuZ <paulomateus.luza@hotmail.com>
2021-10-19 22:31:20 -0300

    [add] quicksort com inserção

--- Algoritimos 2/Trabalho 1/src/quicksort-insercao.c
+++ Algoritimos 2/Trabalho 1/src/quicksort-insercao.c
@@ -3,9 +3,17 @@
 
 
 /* -------------------------------------------------------------------------- */
-/* ordena v[a..b] usando o algoritmo QuickSort com inserção e devolve v       */
+/* ordena v[a..b] usando o algoritmo QuickSort com inserï¿½ï¿½o e devolve v       */
 
 int *quicksort_insercao(int v[], int a, int b, unsigned int m) {
-  
+  if (a >= b) return v;
+  int particionador = particiona(v, a, b, v[b]);
+
+  if (m >= (b-a+1)) {
+    insercao(v, a, b);
+  } else {
+    quicksort_insercao(v, a, particionador-1, m);
+    quicksort_insercao(v, particionador+1, b, m);
+  }
   return v;
 }
