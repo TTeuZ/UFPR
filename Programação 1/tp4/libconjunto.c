57b050fc2bc80145a0f29d765349c1d6ec57381b
TTeuZ <paulomateus.luza@hotmail.com>
2021-11-08 20:32:40 -0300

    [add] 50% do tp4

--- Programação 1/tp4/libconjunto.c
+++ Programação 1/tp4/libconjunto.c
@@ -2,13 +2,106 @@
 #include <stdlib.h>
 #include "libconjunto.h"
 
-conjunto_t * cria_conjunto(int max) {
-    conjunto_t conj;
-    // if (max > MAX) return 0;
-    // else {
-    //     if (*conj.v = malloc(max)) {
-    //         conj.card = max;
-    //         return *conj.v;
-    //     } else return 0;
-    // }
+/* Funções internas */
+
+/* Busca iterativa o elemento x dentro do conjunto.
+Retorna 1 se o elemento foi encontrado e 0 caso ele não esteja no conjunto */
+int busca_binaria(int x, conjunto_t *c) {
+    int begin, end, iterator;
+    begin = 0;
+    end = c->card;
+    while (end > begin) {
+        iterator = (begin + end) / 2;
+        if (*(c->v+iterator) == x) return 1;
+        if (*(c->v+iterator) < x) begin = iterator + 1;
+        else end = iterator;
+    }
+    return 0;
 }
+
+/* Busca para encontrar a posição que o valor indicado deveria estar no vetor,
+para que ele se mantenha ordenado e retorna esta posição.*/
+int find_pos (int x, conjunto_t *c) {
+    int iterator = 0;
+    while (iterator < c->card) {
+        if (x < *(c->v+iterator)) return iterator;
+        iterator++;
+    }
+    return c->card;
+}
+
+/* Insere o elemento x no conjunto, de maneira a manter ele ordenado.
+*/
+void sort_insertion(int x, conjunto_t *c) {
+    int pos, count;
+    pos = find_pos(x, c);
+    for (count = c->card-1; count >= pos; count--) {
+        *(c->v+count+1) = *(c->v+count);
+    }
+    *(c->v+pos) = x;
+}
+
+void sort_remove(int x, conjunto_t *c) {
+    int pos, count;
+    pos = find_pos(x, c);
+    for (count = pos; count <= c->card; count++) {
+        *(c->v+count-1) = *(c->v+count);
+    }
+}
+
+/* Funções internas */
+
+/* Funções publicas */
+
+/* A utilização do (max+1) nos mallocs veio por conta de um erro encontrado pelo valgrind
+De 'invalid write of size 4' */
+conjunto_t * cria_conjunto(int max) {
+    conjunto_t *conj;
+    if ((conj = malloc(sizeof(conjunto_t)*(max+1)))) {
+        conj->max = max;
+        conj->card = 0;
+        conj->v = (int*) malloc(sizeof(int)*(max+1));
+        return conj;
+    } else return 0;
+}
+
+void destroi_conjunto (conjunto_t *c) {
+    free (c->v);
+    free (c);
+}
+
+int cardinalidade (conjunto_t *c) {
+    return c->card;
+}
+
+int eh_vazio (conjunto_t *c) {
+    if (c->card == 0) return 1;
+    else return 0;
+}
+
+int insere (int n, conjunto_t *c) {
+    if (!busca_binaria (n, c)) {
+        if (c->card + 1 > c->max) return -1;
+        else {
+           sort_insertion (n, c); 
+           c->card++;
+           return 1;
+        }
+    } else return 0;
+}
+
+int retira (int n, conjunto_t *c) {
+    if (busca_binaria (n, c) == 1) {
+        sort_remove (n, c); 
+        c->card--;
+        return 1;
+    } else return 0;
+}
+
+void imprime (conjunto_t *c) {
+    int count;
+    for (count = 0; count < c->card; count++) 
+        printf(" %d ", *(c->v+count));
+    printf("\n");
+}
+/* Funções publicas */
--- Programação 1/tp4/teste.c
+++ Programação 1/tp4/teste.c
@@ -2,13 +2,223 @@
 #include <stdlib.h>
 #include "libconjunto.h"
 
-void main () {
-    int *vet, *vet_ex;
-    /* Testando as possibilidades de criações de vetores com valor passando do max
+int main () {
+    conjunto_t *conj, *conj2, *conj3; /* Declaração dos 3 conjuntos */
+    int count; /* Contador para as iterações */
+    int ret; /* variavel para armazenar retornos de funcoes */
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Testando a criação dos conjuntos...\n");
+    printf("\n");
+
+    /* Testando as possibilidades de criações de conjuntos com valor passando do max
      e valor coerente */
-    printf("Testando a criação de vetores...\n");
-    *vet_ex = cria_conjunto(103);
-    printf("O conjunto com valor maior que o max retornou: %p\n", vet_ex);
-    *vet = cria_conjunto(102);
-    printf("O conjunto com valor dentro do limite retornou: %p\n", vet);
+    conj = cria_conjunto(100);
+    printf("O endereço do primeiro conjunto é: %p\n", &conj);
+    conj2 = cria_conjunto(50);
+    printf("O endereço do segundo conjunto é: %p\n", &conj2);
+    conj3 = cria_conjunto(10);
+    printf("O endereço do terceiro conjunto é: %p\n", &conj3);
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("validando que o conjunto está vazio...\n");
+    printf("\n");
+
+    if (eh_vazio(conj) == 1) 
+        printf("O Conjunto 1 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj));
+    else printf("O Conjunto 1 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj));
+    if (eh_vazio(conj2) == 1) 
+        printf("O Conjunto 2 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj2));
+    else printf("O Conjunto 2 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj2));
+    if (eh_vazio(conj3) == 1) 
+        printf("O Conjunto 3 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj3));
+    else printf("O Conjunto 3 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj3));
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("inserindo a primeira metade de elementos dos conjuntos...\n");
+    printf("\n");
+
+    /* Iniciando a inserção da primeira metade de elementos no vetor */
+    for (count = 0; count < (conj->max/2); count++)  /* inserindo elemento somando de 1 em 1 */
+        insere(count, conj);
+    for (count = 0; count < (conj2->max/2); count++) /* inserindo elemento somando de 2 em 2 */
+        insere(count*2, conj2);
+    for (count = 0; count < (conj3->max/2); count++)  /* inserindo elemento somando de 5 em 5 */
+        insere(count*5, conj3);
+
+    printf("------------------------------------------------------------------\n");
+    printf("validando que o conjunto não está vazio...\n");
+    printf("\n");
+
+    if (eh_vazio(conj) == 1) 
+        printf("O Conjunto 1 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj));
+    else printf("O Conjunto 1 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj));
+    if (eh_vazio(conj2) == 1) 
+        printf("O Conjunto 2 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj2));
+    else printf("O Conjunto 2 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj2));
+    if (eh_vazio(conj3) == 1) 
+        printf("O Conjunto 3 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj3));
+    else printf("O Conjunto 3 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj3));
+
+    printf("------------------------------------------------------------------\n");
+    printf("Imprimindo conjuntos apos a inserção da primeira metade...\n");
+    printf("\n");
+
+    printf("Conjunto 1: ");
+    imprime(conj);
+    printf("Conjunto 2: ");
+    imprime(conj2);
+    printf("Conjunto 3: ");
+    imprime(conj3);
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("tentativas de adicionar elementos já existentes...\n");
+    printf("\n");
+
+    for (count = 0; count < 2; count++) { /* inserindo elementos diferentes */
+        ret = insere(count, conj);
+        if (ret == -1) 
+            printf("Não é possivel adicionar o elemento, conjunto cheio, retorno: %d\n", ret);
+        else if(ret == 0)
+            printf("Não é possivel adicionar o elemento, elemento ja existente, retorno: %d\n", ret);
+    }
+    for (count = 0; count < 2; count++) { /* inserindo elementos diferentes */
+        ret = insere(count*2, conj2);
+        if (ret == -1) 
+            printf("Não é possivel adicionar o elemento, conjunto cheio, retorno: %d\n", ret);
+        else if(ret == 0)
+            printf("Não é possivel adicionar o elemento, elemento ja existente, retorno: %d\n", ret);
+    }
+    for (count = 0; count < 2; count++) { /* inserindo elementos diferentes */
+        ret = insere(count*5, conj3);
+        if (ret == -1) 
+            printf("Não é possivel adicionar o elemento, conjunto cheio, retorno: %d\n", ret);
+        else if(ret == 0)
+            printf("Não é possivel adicionar o elemento, elemento ja existente, retorno: %d\n", ret);
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("removendo a primeira metade de elementos dos conjuntos...\n");
+    printf("\n");
+
+    /* Iniciando a remoção da primeira metade de elementos no vetor */
+    for (count = 0; count < (conj->max/2); count++)  /* retirando elemento somando de 1 em 1 */
+        retira(count, conj);
+    for (count = 0; count < (conj2->max/2); count++)  /* retirando elemento somando de 1 em 1 */
+        retira(count*2, conj2);
+    for (count = 0; count < (conj3->max/2); count++)  /* retirando elemento somando de 1 em 1 */
+        retira(count*5, conj3);
+
+    printf("------------------------------------------------------------------\n");
+    printf("validando que o conjunto está novamente vazio...\n");
+    printf("\n");
+
+    if (eh_vazio(conj) == 1) 
+        printf("O Conjunto 1 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj));
+    else printf("O Conjunto 1 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj));
+    if (eh_vazio(conj2) == 1) 
+        printf("O Conjunto 2 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj2));
+    else printf("O Conjunto 2 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj2));
+    if (eh_vazio(conj3) == 1) 
+        printf("O Conjunto 3 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj3));
+    else printf("O Conjunto 3 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj3));
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Imprimindo conjuntos apos a remoção da primeira metade...\n");
+    printf("\n");
+
+    printf("Conjunto 1: ");
+    imprime(conj);
+    printf("Conjunto 2: ");
+    imprime(conj2);
+    printf("Conjunto 3: ");
+    imprime(conj3);
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("inserindo elemento até o max do conjunto...\n");
+    printf("\n");
+
+    /* Iniciando a inserção dos elementoss no conjunto */
+    for (count = 0; count < (conj->max); count++)  /* inserindo elemento somando de 1 em 1, soma */
+        insere(count, conj);
+    for (count = 0; count < (conj2->max); count++) /* inserindo elemento somando de 2 em 2, soma */
+        insere(count*2, conj2);
+    for (count = 0; count < (conj3->max); count++)  /* inserindo elemento somando de 5 em 5, soma */
+        insere(count*5, conj3);
+
+    printf("------------------------------------------------------------------\n");
+    printf("validando que o conjunto não está novamente vazio...\n");
+    printf("\n");
+
+    if (eh_vazio(conj) == 1) 
+        printf("O Conjunto 1 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj));
+    else printf("O Conjunto 1 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj));
+    if (eh_vazio(conj2) == 1) 
+        printf("O Conjunto 2 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj2));
+    else printf("O Conjunto 2 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj2));
+    if (eh_vazio(conj3) == 1) 
+        printf("O Conjunto 3 está vazio!, sua cardinalidade é %d\n", cardinalidade(conj3));
+    else printf("O Conjunto 3 não está vazio!, sua cardinalidade é %d\n", cardinalidade(conj3));
+
+    printf("------------------------------------------------------------------\n");
+    printf("Imprimindo conjuntos apos o preenchimento total...\n");
+    printf("\n");
+
+    printf("Conjunto 1: ");
+    imprime(conj);
+    printf("Conjunto 2: ");
+    imprime(conj2);
+    printf("Conjunto 3: ");
+    imprime(conj3);
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("tentativas de adicionar elementos com os conjuntos cheios (Elementos diferentes)...\n");
+    printf("\n");
+
+    for (count = 0; count < 2; count++) { /* inserindo elementos diferentes */
+        ret = insere((count+1)*100, conj);
+        if (ret == -1) 
+            printf("Não é possivel adicionar o elemento, conjunto cheio, retorno: %d\n", ret);
+        else if(ret == 0)
+            printf("Não é possivel adicionar o elemento, elemento ja existente, retorno: %d\n", ret);
+    }
+    for (count = 0; count < 2; count++) { /* inserindo elementos diferentes */
+        ret = insere((count+1)*100, conj2);
+        if (ret == -1) 
+            printf("Não é possivel adicionar o elemento, conjunto cheio, retorno: %d\n", ret);
+        else if(ret == 0)
+            printf("Não é possivel adicionar o elemento, elemento ja existente, retorno: %d\n", ret);
+    }
+    for (count = 0; count < 2; count++) { /* inserindo elementos diferentes */
+        ret = insere((count+1)*100, conj3);
+        if (ret == -1) 
+            printf("Não é possivel adicionar o elemento, conjunto cheio, retorno: %d\n", ret);
+        else if(ret == 0)
+            printf("Não é possivel adicionar o elemento, elemento ja existente, retorno: %d\n", ret);
+    }
+        
+    printf("------------------------------------------------------------------\n");
+    printf("\n");
+
+    /* Liberando a memoria alocada dinamicamente para os 3 conjuntos durante a execução do teste */
+    printf("Liberando a memoria utilizada pelo conjunto 1...\n");
+    destroi_conjunto(conj);
+    printf("Liberando a memoria utilizada pelo conjunto 2...\n");
+    destroi_conjunto(conj2);
+    printf("Liberando a memoria utilizada pelo conjunto 3...\n");
+    destroi_conjunto(conj3);
+
+    printf("\n");
+    printf("FIM DO TESTE\n");
+    printf("------------------------------------------------------------------\n");
+    printf("------------------------------------------------------------------\n");
+    return 0;
 }
