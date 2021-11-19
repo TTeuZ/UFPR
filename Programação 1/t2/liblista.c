02f80f17eb0c10b86204534dc2adc98320ae0e99
TTeuZ <paulomateus.luza@hotmail.com>
2021-11-19 11:45:04 -0300

    [add] inicio do trabalho 2

--- Programação 1/t2/liblista.c
+++ Programação 1/t2/liblista.c
@@ -1,3 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "liblista.h"
+
+/* Funções internas */
+/* -------------------------------------------------------------------------- */
+
+int insere_lista_vazia(lista_t *l, int elemento) {
+    nodo_t *nodo;
+
+    /* Testa se o malloc ocorreu certo para continuar */
+    if ((nodo = (nodo_t *) malloc (sizeof (nodo_t)))) {
+        nodo->elemento = elemento;
+        /* Seta o prox e o prev para null, por ser o unico elemento atual */
+        nodo->prox = NULL;
+        nodo->prev = NULL;
+
+        /* Aponta o inicio e o fim para este unico nodo */
+        l->ini = nodo;
+        l->fim = nodo;
+        l->tamanho++;
+        return 1;
+    } else return 0;
+}
+
+/* -------------------------------------------------------------------------- */
+/* Funções internas */
+
+/* Funções publicas */
+/* -------------------------------------------------------------------------- */
+/* Necessario inicializar o tamanho por conta do erro */
+/* Conditional jump or move depends on uninitialised value(s) */
+/* que é apontado pelo valgrind quando utilizamos a função 'lista_vazia' para uma lista recem criada. */
+lista_t * cria_lista () {
+    lista_t *list;
+    /* Testa se o malloc da lista deu certo */
+    if ((list = malloc (sizeof (lista_t)))) {
+        list->tamanho = 0;
+        return list;
+    }
+    return NULL;
+}
+
+lista_t * destroi_lista (lista_t *l) {
+    nodo_t *nodo;
+    if (lista_vazia(l)) {
+        free (l); /* Libero o espaço alocado para a estrutura da lista */
+        return NULL;
+    }
+    /* Passa por cada nodo da lista e libera seu espaço */
+    while (l->ini != NULL) {
+        nodo = l->ini;
+        l->ini = l->ini->prox;
+        l->tamanho--;
+        free (nodo);
+    }
+    /* So preciso dar free no l->ini pq nesta situacao, tanto o l->ini e o l->fim estao apontando para o mesmo nodo */
+    /* free (l->ini); */
+    /* free (l); */ /* Libero o espaço alocado para a estrutura da lista */
+    l->tamanho--;
+    return NULL;
+} 
+
+/* retorna 1 se o tamanho for 0, e 0 caso contrario */
+int lista_vazia (lista_t *l) {
+    return l->tamanho == 0;
+}
+
+int tamanho_lista (lista_t *l) {
+    return l->tamanho;
+}
+
+int insere_inicio_lista (lista_t *l, int elemento) {
+    nodo_t *nodo;
+
+    /* Se a minha lista for vazia, chama uma função especifica para esse caso */
+    if (lista_vazia (l)) 
+        return insere_lista_vazia(l, elemento);
+    
+    /* Testa se o malloc ocorreu certo para continuar */
+    if ((nodo = (nodo_t *) malloc (sizeof (nodo_t)))) {
+        /* Aponta o prev do antigo primeiro elemento para o novo primeiro elemento */
+        l->ini->prev = nodo;
+        /* Setando os valores e apontadores do novo nodo */
+        nodo->elemento = elemento;
+        nodo->prox = l->ini; /* Aponta prox para o antigo primeiro elemento */
+        nodo->prev = NULL; /* Aponta prev para NULL */
+        /* Altera o apontador do inicio apenas, por estar inserindo no começo */
+        l->ini = nodo;
+        l->tamanho++;
+        return 1;
+    } else return 0;
+}
+/* -------------------------------------------------------------------------- */
+/* Funções publicas */
--- Programação 1/t2/testa_lista.c
+++ Programação 1/t2/testa_lista.c
@@ -3,6 +3,60 @@
 #include "liblista.h"
 
 int main() {
+    lista_t *list1, *list2; /* Declaração de 2 listas para teste */
+    int count; /* contador para os loops */
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Testando a criação das listas...\n");
+    printf("\n");
+
+    list1 = cria_lista();
+    printf("O endereço da primeira lista é: %p\n", &list1);
+    list2 = cria_lista();
+    printf("O endereço da primeira lista é: %p\n", &list2);
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Confirmando que as listas recem criadas estao vazias...\n");
+    printf("\n");
+
+    if (lista_vazia(list1))
+        printf("A lista 1 esta vazia, seu tamanho é: %d\n", tamanho_lista(list1));
+    else 
+        printf("A lista 1 não esta vazia, seu tamanho é: %d\n", tamanho_lista(list1));
+
+    if (lista_vazia(list2))
+        printf("A lista 2 esta vazia, seu tamanho é: %d\n", tamanho_lista(list2));
+    else 
+        printf("A lista 2 não esta vazia, seu tamanho é: %d\n", tamanho_lista(list2));
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Inserindo elementos no inicio da lista...\n");
+    printf("\n");
+
+    for (count = 1; count <= 3; count++) {
+        if (insere_inicio_lista(list1, count*2)) /* inserindo numeros multiplos de 2 */
+            printf("o numero %d foi inserido corretamente na lista 1, o tamanho atual da lista é: %d\n", count*2, tamanho_lista(list1));
+        else printf("não foi possivel inserir numero %d, o tamanho atual da lista 1 é: %d\n", count*2, tamanho_lista(list1));
+        if (insere_inicio_lista(list2, count*3)) /* inserindo numeros multiplos de 3 */
+            printf("o numero %d foi inserido corretamente na lista 2, o tamanho atual da lista é: %d\n", count*3, tamanho_lista(list1));
+        else printf("não foi possivel inserir numero %d, o tamanho atual da lista 2 é: %d\n", count*3, tamanho_lista(list1));
+        printf("------------------------------------------------------------------\n");
+    }
+
+    printf("\n"); 
+    printf("------------------------------------------------------------------\n");
+    printf("\n");
+
+    printf("Liberando a memoria utilizada pela lista 1...\n");
+    list1 = destroi_lista(list1);
+    printf("lista 1 corretamente liberada, apontando para: %p\n", list1);
+    printf("Liberando a memoria utilizada pela lista 2...\n");
+    list2 = destroi_lista(list2);
+    printf("lista 2 corretamente liberada, apontando para: %p\n", list2);
+
     printf("\n");
     printf("FIM DO TESTE\n");
     printf("------------------------------------------------------------------\n");
