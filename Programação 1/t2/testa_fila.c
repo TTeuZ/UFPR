893ff302970b128bdfd9190acb071d2097584dea
TTeuZ <paulomateus.luza@hotmail.com>
2021-11-18 23:52:04 -0300

    [add] base para o tp2

---
+++ Programação 1/t2/testa_fila.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "libfila.h"
+
+int main() {
+    printf("\n");
+    printf("FIM DO TESTE\n");
+    printf("------------------------------------------------------------------\n");
+    printf("------------------------------------------------------------------\n");
+    return 0;
+}
---
+++ Programação 1/t2/testa_lista.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "liblista.h"
+
+int main() {
+    printf("\n");
+    printf("FIM DO TESTE\n");
+    printf("------------------------------------------------------------------\n");
+    printf("------------------------------------------------------------------\n");
+    return 0;
+}
