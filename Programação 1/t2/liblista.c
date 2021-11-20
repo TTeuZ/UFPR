5f12ce724ca500f8c11dcf66743dd8d7cb7de325
TTeuZ <paulomateus.luza@hotmail.com>
2021-11-19 23:04:44 -0300

    [add] finalização do tad de lista

--- Programação 1/t2/liblista.c
+++ Programação 1/t2/liblista.c
@@ -4,17 +4,14 @@
 
 /* Funções internas */
 /* -------------------------------------------------------------------------- */
-
 int insere_lista_vazia(lista_t *l, int elemento) {
     nodo_t *nodo;
-
     /* Testa se o malloc ocorreu certo para continuar */
     if ((nodo = (nodo_t *) malloc (sizeof (nodo_t)))) {
         nodo->elemento = elemento;
         /* Seta o prox e o prev para null, por ser o unico elemento atual */
         nodo->prox = NULL;
         nodo->prev = NULL;
-
         /* Aponta o inicio e o fim para este unico nodo */
         l->ini = nodo;
         l->fim = nodo;
@@ -22,7 +19,6 @@ int insere_lista_vazia(lista_t *l, int elemento) {
         return 1;
     } else return 0;
 }
-
 /* -------------------------------------------------------------------------- */
 /* Funções internas */
 
@@ -55,9 +51,8 @@ lista_t * destroi_lista (lista_t *l) {
         free (nodo);
     }
     /* So preciso dar free no l->ini pq nesta situacao, tanto o l->ini e o l->fim estao apontando para o mesmo nodo */
-    /* free (l->ini); */
-    /* free (l); */ /* Libero o espaço alocado para a estrutura da lista */
     l->tamanho--;
+    free (l); /* Libero o espaço alocado para a estrutura da lista */
     return NULL;
 } 
 
@@ -72,24 +67,147 @@ int tamanho_lista (lista_t *l) {
 
 int insere_inicio_lista (lista_t *l, int elemento) {
     nodo_t *nodo;
-
     /* Se a minha lista for vazia, chama uma função especifica para esse caso */
-    if (lista_vazia (l)) 
-        return insere_lista_vazia(l, elemento);
-    
+    if (lista_vazia (l)) return insere_lista_vazia(l, elemento);
     /* Testa se o malloc ocorreu certo para continuar */
     if ((nodo = (nodo_t *) malloc (sizeof (nodo_t)))) {
-        /* Aponta o prev do antigo primeiro elemento para o novo primeiro elemento */
-        l->ini->prev = nodo;
         /* Setando os valores e apontadores do novo nodo */
         nodo->elemento = elemento;
         nodo->prox = l->ini; /* Aponta prox para o antigo primeiro elemento */
         nodo->prev = NULL; /* Aponta prev para NULL */
-        /* Altera o apontador do inicio apenas, por estar inserindo no começo */
+        /* Aponta o prev do 'antigo primeiro elemento' para o 'novo primeiro elemento' */
+        l->ini->prev = nodo;
+        /* Altera o apontador do inicio para o nodo (insirindo no começo) */
         l->ini = nodo;
         l->tamanho++;
         return 1;
     } else return 0;
+}
+
+int insere_fim_lista (lista_t *l, int elemento) {
+    nodo_t *nodo;
+    /* Se a minha lista for vazia, chama uma função especifica para esse caso */
+    if (lista_vazia (l)) return insere_lista_vazia(l, elemento);
+    /* Testa se o malloc ocorreu certo para continuar */
+    if ((nodo = (nodo_t *) malloc (sizeof (nodo_t)))) {
+        /* Setando os valores e apontadores do novo nodo */
+        nodo->elemento = elemento;
+        nodo->prox = NULL; /* Aponta prox para NULL por ser o ultimo elemento */
+        nodo->prev = l->fim; /* Aponta o prev para o antigo ulitmo elemento */
+        /* Aponta o prox do 'antigo ultimo elemento' para o 'novo ultimo elemento' */
+        l->fim->prox = nodo;
+        /* Altera o apontador do fim para o nodo (inserindo no final) */
+        l->fim = nodo;
+        l->tamanho++;
+        return 1;
+    } else return 0;
+}
+
+int insere_ordenado_lista (lista_t *l, int elemento) {
+    nodo_t *nodo, *nodo_ante, *nodo_atual;
+    /* Se a minha lista for vazia, chama uma função especifica para esse caso */
+    if (lista_vazia (l)) return insere_lista_vazia(l, elemento);    
+
+    nodo_atual = l->ini;
+    /* While par encontrar a posicao que o elemento deve ser inserido */
+    while (nodo_atual->prox != NULL && nodo_atual->elemento <= elemento) {
+        nodo_ante = nodo_atual;
+        nodo_atual = nodo_atual->prox;
+    }
+    /* Se o elemento for menor que todos os demais, insere no inicio. Verifica
+    se o nodo->elemento é menor que o elemento pois no caso de termos apenas
+    1 item na lista, tando o l->ini e o l->fim apontam para o mesmo nodo */
+    if (nodo_atual == l->ini && nodo_atual->elemento > elemento) 
+        return insere_inicio_lista (l, elemento);
+    /* Se o elemento for maior que todos os demais, insere no fim */
+    else if (nodo_atual == l->fim) return insere_fim_lista (l, elemento);
+    /* insere o elemento entre o anterior e o prox*/
+    else {
+        if ((nodo = (nodo_t *) malloc (sizeof (nodo_t)))) {
+            nodo->elemento = elemento;
+            /* vinculos com o elemento posterior */
+            nodo->prox = nodo_atual;
+            nodo_atual->prev = nodo;
+            /* vinculos com o elementoanterior */
+            nodo->prev = nodo_ante;
+            nodo_ante->prox = nodo;
+            l->tamanho++;
+            return 1;
+        } else return 0;
+    }
+}
+
+int retira_inicio_lista (lista_t *l, int *elemento) {
+    nodo_t *nodo;
+    /* Se a lista estiver vazia retorna 0 */
+    if (lista_vazia(l)) return 0;
+    nodo = l->ini; /* Armazena o antigo nodo de inicio */
+    l->ini = nodo->prox; /* Aponta o ini para o prox nodo */
+    /* Se a lista nao tiver ficado vazia, aponta o prev para NULL */
+    if (l->ini != NULL) 
+        l->ini->prev = NULL;
+    *elemento = nodo->elemento; /* Retorna o elemento removido */
+    free (nodo);
+    l->tamanho--;
+    return 1;
+}
+
+int retira_fim_lista (lista_t *l, int *elemento) {
+    nodo_t *nodo;
+    /* Se a lista estiver vazia retorna 0 */
+    if (lista_vazia(l)) return 0;
+    nodo = l->fim; /* Armazena o antigo nodo de fim */
+    l->fim = nodo->prev; /* Aponta o fim para o nodo anterior */
+    /* Se a lista nao tiver ficado vazia, aponta o prox para NULL */
+    if (l->fim != NULL) 
+        l->fim->prox = NULL;
+    *elemento = nodo->elemento; /* Retorna o elemento removido */
+    free (nodo);
+    l->tamanho--;
+    return 1;
+}
+
+int retira_elemento_lista (lista_t *l, int elemento) {
+    nodo_t *nodo;
+    int temp; /* inteiro para receber os retornos do retira inicio/fim (nao utilizado) */
+    /* Se a lista esta vazia, retorna 0 */
+    if (lista_vazia(l)) return 0;
+
+    nodo = l->ini;
+    /* While par encontrar a posicao que o elemento deve ser inserido */
+    while (nodo->prox != NULL && nodo->elemento != elemento)
+        nodo = nodo->prox;
+
+    /* Verifica se o elemento foi encontrado, ou se o lista chegou no fim */
+    if (nodo->elemento == elemento) {
+        /* se o elemento estiver no inicio, chama a funcao para retirar do inicio */
+        if (nodo == l->ini) return retira_inicio_lista (l, &temp);
+        /* Se o elemento estiver no fim, chama a funcao para retirar do fim */
+        else if (nodo == l->fim) return retira_fim_lista (l, &temp);
+        /* Se o elemento estiver no meio */
+        else {
+            /* faz o prev do nodo da frente apontar para o prev do nodo que vai ser removido */
+            nodo->prox->prev = nodo->prev;
+            /* faz o prro do nodo de traz apontar para o prox do nodo que vai ser removido */
+            nodo->prev->prox = nodo->prox;
+            l->tamanho--;
+            free (nodo);
+            return 1;
+        }
+    } else return 0;
+}
+
+int pertence_lista (lista_t *l, int elemento) {
+    nodo_t *nodo;
+    /* Se a lista esta vazia, retorna 0 */
+    if (lista_vazia(l)) return 0;
+    nodo = l->ini; /* Inicializa com a primeira posicao */
+    /* Passa por todos os elementos da lista, até seu final, ou até encontrar um nodo com o mesmo elemento procurado */
+    while (nodo != NULL && nodo->elemento != elemento) 
+        nodo = nodo->prox;
+    
+    if (nodo == NULL) return 0; /* Se chegou no fim e não encontrou retorna 0*/
+    return 1; /* Caso o nodo seja diferente de NULL, retorna 1 */
 }
 /* -------------------------------------------------------------------------- */
 /* Funções publicas */
--- Programação 1/t2/testa_lista.c
+++ Programação 1/t2/testa_lista.c
@@ -3,59 +3,279 @@
 #include "liblista.h"
 
 int main() {
-    lista_t *list1, *list2; /* Declaração de 2 listas para teste */
+    lista_t *list1, *list2, *list3; /* Declaração de 2 listas para teste */
+    int elemento; /* Variavel que recebe os elementos removidos das listas */
     int count; /* contador para os loops */
 
     printf("\n");
     printf("------------------------------------------------------------------\n");
-    printf("Testando a criação das listas...\n");
+    printf("Testando a criacao das listas...\n");
     printf("\n");
 
     list1 = cria_lista();
-    printf("O endereço da primeira lista é: %p\n", &list1);
+    printf("O endereço da primeira lista eh: %p\n", &list1);
     list2 = cria_lista();
-    printf("O endereço da primeira lista é: %p\n", &list2);
+    printf("O endereço da primeira lista eh: %p\n", &list2);
+    list3 = cria_lista();
+    printf("O endereço da primeira lista eh: %p\n", &list3);
 
     printf("\n");
     printf("------------------------------------------------------------------\n");
     printf("Confirmando que as listas recem criadas estao vazias...\n");
     printf("\n");
 
-    if (lista_vazia(list1))
-        printf("A lista 1 esta vazia, seu tamanho é: %d\n", tamanho_lista(list1));
+    if (lista_vazia (list1))
+        printf("A lista 1 esta vazia, seu tamanho eh: %d\n", tamanho_lista(list1));
     else 
-        printf("A lista 1 não esta vazia, seu tamanho é: %d\n", tamanho_lista(list1));
+        printf("A lista 1 nao esta vazia, seu tamanho eh: %d\n", tamanho_lista(list1));
 
-    if (lista_vazia(list2))
-        printf("A lista 2 esta vazia, seu tamanho é: %d\n", tamanho_lista(list2));
+    if (lista_vazia (list2))
+        printf("A lista 2 esta vazia, seu tamanho eh: %d\n", tamanho_lista(list2));
     else 
-        printf("A lista 2 não esta vazia, seu tamanho é: %d\n", tamanho_lista(list2));
+        printf("A lista 2 nao esta vazia, seu tamanho eh: %d\n", tamanho_lista(list2));
+
+    if (lista_vazia (list3))
+        printf("A lista 2 esta vazia, seu tamanho eh: %d\n", tamanho_lista(list3));
+    else 
+        printf("A lista 2 nao esta vazia, seu tamanho eh: %d\n", tamanho_lista(list3));
 
     printf("\n");
     printf("------------------------------------------------------------------\n");
-    printf("Inserindo elementos no inicio da lista...\n");
+    printf("Inserindo elementos pelo inicio da lista 1...\n");
     printf("\n");
 
     for (count = 1; count <= 3; count++) {
-        if (insere_inicio_lista(list1, count*2)) /* inserindo numeros multiplos de 2 */
-            printf("o numero %d foi inserido corretamente na lista 1, o tamanho atual da lista é: %d\n", count*2, tamanho_lista(list1));
-        else printf("não foi possivel inserir numero %d, o tamanho atual da lista 1 é: %d\n", count*2, tamanho_lista(list1));
-        if (insere_inicio_lista(list2, count*3)) /* inserindo numeros multiplos de 3 */
-            printf("o numero %d foi inserido corretamente na lista 2, o tamanho atual da lista é: %d\n", count*3, tamanho_lista(list1));
-        else printf("não foi possivel inserir numero %d, o tamanho atual da lista 2 é: %d\n", count*3, tamanho_lista(list1));
-        printf("------------------------------------------------------------------\n");
+        if (insere_inicio_lista (list1, count*2)) /* inserindo numeros multiplos de 2 */
+            printf("O numero %d foi inserido corretamente na lista 1, o tamanho atual da lista é: %d\n", count*2, tamanho_lista(list1));
+        else printf("Nao foi possivel inserir numero %d, o tamanho atual da lista 1 é: %d\n", count*2, tamanho_lista(list1));
     }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Inserindo elementos pelo fim da lista 2...\n");
+    printf("\n");
+
+    for (count = 1; count <= 3; count++) {
+        if (insere_fim_lista (list2, count*3)) /* inserindo numeros multiplos de 3 */
+            printf("O numero %d foi inserido corretamente na lista 2, o tamanho atual da lista é: %d\n", count*3, tamanho_lista(list2));
+        else printf("Nao foi possivel inserir numero %d, o tamanho atual da lista 2 é: %d\n", count*3, tamanho_lista(list2));
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Verificando que os elementos recem adicionados realmente estão na lista...\n");
+    printf("\n");
+
+    for (count = 1; count <= 3; count++) {
+        if (pertence_lista (list1, count*2)) /* inserindo numeros multiplos de 2 */
+            printf("O numero %d esta presente na lista 1!\n", count*2);
+        else printf("O numero %d nao esta presente na lista 1!\n", count*2);
+    }
+    printf("------------------------------------------------------------------\n");
+    for (count = 1; count <= 3; count++) {
+        if (pertence_lista (list2, count*3)) /* inserindo numeros multiplos de 2 */
+            printf("O numero %d esta presente na lista 2!\n", count*3);
+        else printf("O numero %d nao esta presente na lista 2!\n", count*3);
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Inserindo elementos pelo fim da lista 1 (continuando a contagem)...\n");
+    printf("\n");
+
+    for (count = 4; count <= 6; count++) {
+        if (insere_fim_lista (list1, count*2)) /* inserindo numeros multiplos de 2 */
+            printf("O numero %d foi inserido corretamente na lista 1, o tamanho atual da lista é: %d\n", count*2, tamanho_lista(list1));
+        else printf("Nao foi possivel inserir numero %d, o tamanho atual da lista 1 é: %d\n", count*2, tamanho_lista(list1));
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Inserindo elementos pelo inicio da lista 2 (continuando a contagem)...\n");
+    printf("\n");
+
+    for (count = 4; count <= 6; count++) {
+        if (insere_inicio_lista (list2, count*3)) /* inserindo numeros multiplos de 3 */
+            printf("O numero %d foi inserido corretamente na lista 2, o tamanho atual da lista é: %d\n", count*3, tamanho_lista(list2));
+        else printf("Nao foi possivel inserir numero %d, o tamanho atual da lista 2 é: %d\n", count*3, tamanho_lista(list2));
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Verificando que todos os elementos adicionados ate o momentos estao na lista...\n");
+    printf("\n");
+
+    for (count = 1; count <= 6; count++) {
+        if (pertence_lista (list1, count*2)) /* inserindo numeros multiplos de 2 */
+            printf("O numero %d esta presente na lista 1!\n", count*2);
+        else printf("O numero %d nao esta presente na lista 1!\n", count*2);
+    }
+    printf("------------------------------------------------------------------\n");
+    for (count = 1; count <= 6; count++) {
+        if (pertence_lista (list2, count*3)) /* inserindo numeros multiplos de 2 */
+            printf("O numero %d esta presente na lista 2!\n", count*3);
+        else printf("O numero %d nao esta presente na lista 2!\n", count*3);
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Verificando se elementos nao adicionados estao na lista...\n");
+    printf("\n");
+
+    for (count = 1; count <= 3; count++) {
+        if (pertence_lista (list1, count*11)) /* inserindo numeros multiplos de 11 */
+            printf("O numero %d esta presente na lista 2!\n", count*11);
+        else printf("O numero %d nao esta presente na lista 2!\n", count*11);
+    }
+    printf("------------------------------------------------------------------\n");
+    for (count = 1; count <= 3; count++) {
+        if (pertence_lista (list2, count*11)) /* inserindo numeros multiplos de 11 */
+            printf("O numero %d esta presente na lista 2!\n", count*11);
+        else printf("O numero %d nao esta presente na lista 2!\n", count*11);
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Inserindo elementos de maneira ordenada na lista 3...\n");
+    printf("\n");
+
+    printf("Inserindo elementos 25 e 30 na lista 3...\n");
+    for (count = 5; count <= 6; count++) {
+        if (insere_ordenado_lista (list3, count*5)) /* inserindo numeros multiplos de 5 */
+            printf("O numero %d foi inserido corretamente na lista 3, o tamanho atual da lista é: %d\n", count*5, tamanho_lista(list3));
+        else printf("Nao foi possivel inserir numero %d, o tamanho atual da lista 3 é: %d\n", count*5, tamanho_lista(list3));
+    }
+    printf("------------------------------------------------------------------\n");
+    printf("Inserindo elementos 5 e 10 na lista 3...\n");
+    for (count = 1; count <= 2; count++) {
+        if (insere_ordenado_lista (list3, count*5)) /* inserindo numeros multiplos de 5 */
+            printf("O numero %d foi inserido corretamente na lista 3, o tamanho atual da lista é: %d\n", count*5, tamanho_lista(list3));
+        else printf("Nao foi possivel inserir numero %d, o tamanho atual da lista 3 é: %d\n", count*5, tamanho_lista(list3));
+    }
+    printf("------------------------------------------------------------------\n");
+    printf("Inserindo elementos 15 e 20 na lista 3...\n");
+    for (count = 3; count <= 4; count++) {
+        if (insere_ordenado_lista (list3, count*5)) /* inserindo numeros multiplos de 5 */
+            printf("O numero %d foi inserido corretamente na lista 3, o tamanho atual da lista é: %d\n", count*5, tamanho_lista(list3));
+        else printf("Nao foi possivel inserir numero %d, o tamanho atual da lista 3 é: %d\n", count*5, tamanho_lista(list3));
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Verificando que todos os elementos adicionados ate o momento estao na lista 3...\n");
+    printf("\n");
+
+    for (count = 1; count <= 6; count++) {
+        if (pertence_lista (list3, count*5)) /* inserindo numeros multiplos de 5 */
+            printf("O numero %d esta presente na lista 2!\n", count*5);
+        else printf("O numero %d nao esta presente na lista 2!\n", count*5);
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Retirando os 3 primeiros elementos da lista 1...\n");
+    printf("\n");
+
+    for (count = 1; count <= 3; count++) {
+        if (retira_inicio_lista (list1, &elemento)) /* inserindo numeros multiplos de 5 */
+            printf("O numero %d foi removido da lista 1! O tamanhao da lista eh: %d\n", elemento, tamanho_lista(list1));
+        else printf("Nao foi possivel remover o numero %d da lista 1\n", elemento);
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Retirando os 3 ultimos elementos da lista 2...\n");
+    printf("\n");
+
+    for (count = 1; count <= 3; count++) {
+        if (retira_fim_lista (list2, &elemento)) /* inserindo numeros multiplos de 5 */
+            printf("O numero %d foi removido da lista 2! O tamanhao da lista eh: %d\n", elemento, tamanho_lista(list2));
+        else printf("Nao foi possivel remover o numero %d da lista 2\n", elemento);
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Retirando os 3 ultimos elementos da lista 1...\n");
+    printf("\n");
+
+    for (count = 1; count <= 3; count++) {
+        if (retira_fim_lista (list1, &elemento)) /* inserindo numeros multiplos de 5 */
+            printf("O numero %d foi removido da lista 1! O tamanhao da lista eh: %d\n", elemento, tamanho_lista(list1));
+        else printf("Nao foi possivel remover o numero %d da lista 1\n", elemento);
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Retirando os 3 primeiros elementos da lista 2...\n");
+    printf("\n");
+
+    for (count = 1; count <= 3; count++) {
+        if (retira_inicio_lista (list2, &elemento)) /* inserindo numeros multiplos de 5 */
+            printf("O numero %d foi removido da lista 2! O tamanhao da lista eh: %d\n", elemento, tamanho_lista(list2));
+        else printf("Nao foi possivel remover o numero %d da lista 2\n", elemento);
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Validando que a lista 1 e 2 estao vazias...\n");
+    printf("\n");
+
+    if (lista_vazia (list1))
+        printf("A lista 1 esta vazia, seu tamanho eh: %d\n", tamanho_lista(list1));
+    else 
+        printf("A lista 1 nao esta vazia, seu tamanho eh: %d\n", tamanho_lista(list1));
+
+    if (lista_vazia (list2))
+        printf("A lista 2 esta vazia, seu tamanho eh: %d\n", tamanho_lista(list2));
+    else 
+        printf("A lista 2 nao esta vazia, seu tamanho eh: %d\n", tamanho_lista(list2));
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("retirando elementos alternados na lista 3...\n");
+    printf("\n");
+
+    for (count = 3; count <= 4; count++) {
+        if (retira_elemento_lista (list3, count*5)) /* inserindo numeros multiplos de 5 */
+            printf("O numero %d foi removido corretamente da lista 3, o tamanho atual da lista é: %d\n", count*5, tamanho_lista(list3));
+        else printf("Nao foi possivel remover numero %d, o tamanho atual da lista 3 é: %d\n", count*5, tamanho_lista(list3));
+    }
+    printf("------------------------------------------------------------------\n");
+    for (count = 5; count <= 6; count++) {
+        if (retira_elemento_lista (list3, count*5)) /* inserindo numeros multiplos de 5 */
+            printf("O numero %d foi removido corretamente da lista 3, o tamanho atual da lista é: %d\n", count*5, tamanho_lista(list3));
+        else printf("Nao foi possivel remover numero %d, o tamanho atual da lista 3 é: %d\n", count*5, tamanho_lista(list3));
+    }
+    printf("------------------------------------------------------------------\n");
+    for (count = 1; count <= 2; count++) {
+        if (retira_elemento_lista (list3, count*5)) /* inserindo numeros multiplos de 5 */
+            printf("O numero %d foi removido corretamente da lista 3, o tamanho atual da lista é: %d\n", count*5, tamanho_lista(list3));
+        else printf("Nao foi possivel remover numero %d, o tamanho atual da lista 3 é: %d\n", count*5, tamanho_lista(list3));
+    }
+
+    printf("\n");
+    printf("------------------------------------------------------------------\n");
+    printf("Validando que a lista 3 esta vazia...\n");
+    printf("\n");
+
+    if (lista_vazia (list3))
+        printf("A lista 2 esta vazia, seu tamanho eh: %d\n", tamanho_lista(list3));
+    else 
+        printf("A lista 2 nao esta vazia, seu tamanho eh: %d\n", tamanho_lista(list3));
 
     printf("\n"); 
     printf("------------------------------------------------------------------\n");
     printf("\n");
 
     printf("Liberando a memoria utilizada pela lista 1...\n");
-    list1 = destroi_lista(list1);
+    list1 = destroi_lista (list1);
     printf("lista 1 corretamente liberada, apontando para: %p\n", list1);
     printf("Liberando a memoria utilizada pela lista 2...\n");
-    list2 = destroi_lista(list2);
+    list2 = destroi_lista (list2);
     printf("lista 2 corretamente liberada, apontando para: %p\n", list2);
+    printf("Liberando a memoria utilizada pela lista 3...\n");
+    list3 = destroi_lista (list3);
+    printf("lista 2 corretamente liberada, apontando para: %p\n", list3);
 
     printf("\n");
     printf("FIM DO TESTE\n");
