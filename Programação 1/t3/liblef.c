9b3061bbe3a33c32753f5975dafee39b978994fc
TTeuZ <paulomateus.luza@hotmail.com>
2021-11-24 17:19:37 -0300

    [add] finalização da liblef

--- Programação 1/t3/liblef.c
+++ Programação 1/t3/liblef.c
@@ -24,7 +24,8 @@ lef_t * destroi_lef (lef_t *l) {
     /* Alquanto a lista não ficar vazia, vai liberando os nodos */
     while (l->Primeiro != NULL) {
         nodo = l->Primeiro;
-        l->Primeiro = l->Primeiro->prox; /* aponta o primeiro para o prox elemento */
+        l->Primeiro = l->Primeiro->prox;
+        free (nodo->evento); /* aponta o primeiro para o prox elemento */
         free (nodo); /* libera o espaço do nodo */
     }
     /* Libera o espaço da estrutura da lista ao final */
@@ -34,21 +35,76 @@ lef_t * destroi_lef (lef_t *l) {
 
 int adiciona_inicio_lef (lef_t *l, evento_t *evento) {
     nodo_lef_t *nodo;
+    evento_t *evento_copy;
+    /* Cria uma copia do evento */
+    if ((evento_copy = malloc (sizeof(evento_t)))) {
+        evento_copy->tempo = evento->tempo;
+        evento_copy->tipo = evento->tipo;
+        evento_copy->tamanho = evento->tamanho;
+        evento_copy->dados = evento->dados;
+    } else return 0;
     /* Verifica se o malloc para o nodo deu boa */
     if ((nodo =  malloc (sizeof (nodo_lef_t)))) {
-        nodo->evento = evento; /* seta o valor do evento */
+        nodo->evento = evento_copy; /* seta o valor do evento */
         nodo->prox = NULL; /* Seta o prox do nodo como null */
     } else return 0;
     /* Se a lef estiver vazia, apenas da free em sua estrutura */
-    if (l->Primeiro == NULL) {
+    if (l->Primeiro == NULL) 
         l->Primeiro = nodo;
-    } 
     /* Se estiver vazia, altera a sequencia dos ponteiros */ 
     else {
         nodo->prox = l->Primeiro;
         l->Primeiro = nodo;
     }
     return 1;
+}
+
+void adiciona_ordem_lef  (lef_t *l, evento_t *evento) {
+    nodo_lef_t *nodo, *temp_ante, *temp_atual;
+    evento_t *evento_copy;
+    /* Cria uma copia do evento */
+    if ((evento_copy = malloc (sizeof(evento_t)))) {
+        evento_copy->tempo = evento->tempo;
+        evento_copy->tipo = evento->tipo;
+        evento_copy->tamanho = evento->tamanho;
+        evento_copy->dados = evento->dados;
+    } else return;
+
+    temp_atual = l->Primeiro;
+    /* While par encontrar a posicao que o elemento deve ser inserido */
+    while (temp_atual->prox != NULL && temp_atual->evento->tempo >= evento->tempo) {
+        temp_ante = temp_atual;
+        temp_atual = temp_atual->prox;
+    }
+
+    /* Se a posição encontrada for a primeira, insere no inicio */
+    if (temp_atual == l->Primeiro) {
+        adiciona_inicio_lef (l, evento);
+        return;
+    } else {
+        /* Verifica se o malloc para o nodo deu boa */
+        if ((nodo =  malloc (sizeof (nodo_lef_t)))) {
+            nodo->evento = evento_copy; /* seta o valor do evento */
+
+            temp_ante->prox = nodo; /* Aponta o prox do anterior para o noco nodo */
+            nodo->prox = temp_atual; /* aponta o prox do novo nodo para o nodo da seguencia */
+            return;
+        } else return;
+    }
+}
+
+evento_t * obtem_primeiro_lef (lef_t *l) {
+    nodo_lef_t *nodo;
+    evento_t *evento;
+    
+    if (!l->Primeiro) return NULL; /* Se a lista ficou vazia retorna null */
+
+    evento = l->Primeiro->evento; /* recebe o evento atual */
+    nodo = l->Primeiro; /* armazena o nodo que tinha o evento */
+
+    l->Primeiro = l->Primeiro->prox; /* aponta o ponteiro do primeiro para o prox da lista */
+    free (nodo); /* libera o espaço do nodo */
+    return evento; /* retorna o evento */
 }
 /* -------------------------------------------------------------------------- */
 /* Funções publicas */
