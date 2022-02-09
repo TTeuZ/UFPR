a6ecfb3a1f049501ebde10596ef327907af9d4af
TTeuZ <paulomateus.luza@hotmail.com>
2022-02-09 00:46:26 -0300

    [add] base 1 pra os filtros

--- Programação 2/projeto1/pgmmedia.c
+++ Programação 2/projeto1/pgmmedia.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "core_functions.h"
+#include "image_functions.h"
 
 int main (int argc, char *argv[]) {
-    printf("tamo no média\n");
+    image_f *image;
+    paramns_f *paramns;
+
+    /* Inicializa as imagens e os parametros */
+    image = initialize_image ();
+    paramns = initialize_paramns ();
+
+    treat_paramns (argv, argc, paramns);
+
+    printf("endereço da imagem: %p\n", &image);
+    printf("endereço dos parametros: %p\n", &paramns);
+
+    /* Libera todo o espaço alocado */
+    clean_structs (image, paramns);
+
     return 1;
 }
