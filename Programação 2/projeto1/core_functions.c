dc543206299d3156312ffdcd742925a9d76b943c
TTeuZ <paulomateus.luza@hotmail.com>
2022-02-09 00:47:12 -0300

    [add] primieras cores_functions

--- Programação 2/projeto1/core_functions.c
+++ Programação 2/projeto1/core_functions.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "core_functions.h"
+
+#define RED "\e[0;31m"
+#define NC "\e[0m"
+
+void emit_error(char *message) {
+    fprintf(stderr, RED "[ERROR]\n"
+    NC "%s\n", message);
+}
+
+void clean_structs (image_f *image, paramns_f *paramns) {
+    free (image);
+    free (paramns);
+}
+
+image_f *initialize_image () {
+    image_f *image;
+    if (!(image = malloc (sizeof(image_f)))) {
+        emit_error("Falha na alocação de memória para a imagem!\n");
+        exit(EXIT_FAILURE);
+    } else return image;
+}
+
+paramns_f *initialize_paramns () {
+    paramns_f *paramns;
+    if (!(paramns = malloc (sizeof(paramns_f)))) {
+        emit_error("Falha na alocação de memória para os parametros!\n");
+        exit(EXIT_FAILURE);
+    } else return paramns;
+}
+
+/*
+* Recebe o argv e o argc como parametros, e varre eles armazenando os parametros
+*/
+void treat_paramns (char *paramns[], int qtd, paramns_f *param) {
+    int count;
+    for (count = 0; count < qtd; count++) {
+        printf("%s\n", paramns[count]);
+    }
+}
--- Programação 2/projeto1/core_functions.h
+++ Programação 2/projeto1/core_functions.h
@@ -1,21 +1,26 @@
 #include "entities.h"
 
 /*
+* Função pardrao para exibição de erros
+*/
+void emit_error (char *message);
+
+/*
+* Limpa todos os malocs feitos durante a execução do filtro.
+*/
+void clean_structs (image_f *image, paramns_f *paramns);
+
+/*
+* Inicializa a struct da imagem utilizada no filtro (Não faz maloc para a matrix da imagem)
+*/
+image_f *initialize_image ();
+
+/*
+* Inicializa a struct dos parametros do filtro.
+*/
+paramns_f *initialize_paramns ();
+
+/*
 * Função que ajusta os parametros enviados enviados na chamada do filtro
 */
-void treat_paramns (char *paramns, int qtd);
-
-/*
-* Inicia as estruturas bases que vão ser usadas nos filtros
-*/
-void iniciate_filter (image *image, paramns paramns);
-
-/*
-* Função que faz a leitura e constroi o imagem que vai ser utilizada no filtro
-*/
-image read_image ();
-
-/*
-* Função que retorna a nova imagem construida para o output
-*/
-void send_image ();
+void treat_paramns (char *paramns[], int qtd, paramns_f *param);
