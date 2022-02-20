110d23d6d017a5b0fcbdd080f7fdb4fea8a4892f
TTeuZ <paulomateus.luza@hotmail.com>
2022-02-20 19:59:23 -0300

    [add] função de leitura de p2

--- Programação 2/projeto1/image_functions.c
+++ Programação 2/projeto1/image_functions.c
@@ -3,11 +3,34 @@
 #include <string.h>
 #include "image_functions.h"
 #include "core_functions.h"
+
 /*
 * Função responsavel por ler e armazenas as informações da imagem de tipo P2
 */
 void read_p2_file (image_f *image, params_f *params, FILE *image_r) {
-    printf("Boura ler um p2\n");
+    int row, col, value;
+
+    fprintf(stdout, GREEN "[PROCESSANDO] "  NC "Lendo a imagem enviada...\n\n");
+
+    while(getc(image_r) != '\n'); /* Vai até o fim da linha */
+    while (getc(image_r) == '#') { /* Pula os comentarios */
+        while (getc(image_r) != '\n'); /* Vai até o fim da linha */
+    }
+    fseek (image_r, -1, SEEK_CUR); /* volta um caracter para traz */
+    /* armazena as informações da estrutura da pgm */
+    fscanf (image_r, "%d", &image->width);
+    fscanf (image_r, "%d", &image->height);
+    fscanf (image_r, "%d", &image->max_value);
+    
+    /* le a matriz da imagem e armazena na struct */
+    if ((image->data = malloc (sizeof (image->data) * image->height * image->width))) {
+        for (row = 0; row < image->height; row++)
+            for (col = 0; col < image->height; col++) {
+                fscanf(image_r,"%d", &value);
+                image->data[(row * image->height) + col] = value;
+            } 
+    } else emit_error (image, params, "Falha na alocação de memória para o data da imagem!\n");
+
     fclose (image_r);
 }
 
@@ -15,7 +38,11 @@ void read_p2_file (image_f *image, params_f *params, FILE *image_r) {
 * Função responsavel por ler e armazenas as informações da imagem de tipo P5
 */
 void read_p5_file (image_f *image, params_f *params, FILE *image_r) {
-    printf("Boura ler um p5\n");
+    int teste;
+    fscanf (image_r, "%d", &teste);
+    printf("Boura ler um p5: %d\n", teste);
+    fscanf (image_r, "%d", &teste);
+    printf("Boura ler um p5: %d\n", teste);
     fclose (image_r);
 }
 
@@ -35,9 +62,7 @@ void read_image (image_f *image, params_f *params, char *param[]) {
     if (strcmp (image->image_type, "P2") && strcmp (image->image_type, "P5")) {
         fclose (image_r);
         emit_error (image, params, "O tipo de imagem não é compátivel!");
-    } else {
-        if (! strcmp (image->image_type, "P2")) 
-            read_p2_file (image, params, image_r);
-        else read_p5_file (image, params, image_r);
-    }
+    } else if (! strcmp (image->image_type, "P2")) 
+        read_p2_file (image, params, image_r);
+    else read_p5_file (image, params, image_r);
 }
