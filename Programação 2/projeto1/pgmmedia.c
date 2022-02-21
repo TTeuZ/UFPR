a6f73be8e44dd0204fe7ad9aa9cff57a9080a45b
TTeuZ <paulomateus.luza@hotmail.com>
2022-02-20 22:47:18 -0300

    [add] estrutura padrão do filtro

--- Programação 2/projeto1/pgmmedia.c
+++ Programação 2/projeto1/pgmmedia.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "core_functions.h"
+#include "image_functions.h"
 
 int main (int argc, char *argv[]) {
+    image_f *image;
+    params_f *params;
+
+    /* Inicializa as imagens e os parametros */
+    image = initialize_image ();
+    params = initialize_params (image);
+
+    /* tratamento dos parametros enviados para o filtro */
+    treat_params (image, params, "", 0, argv, argc);
+
+    /* chama a função que lê o pgm */
+    read_image (image, params, argv);
+
+    /* chama a função que grava a nova pgm */
+    send_image (image, params, argv);
+
+    /* Libera todo o espaço alocado */
+    clean_structs (image, params);
+
     exit (EXIT_SUCCESS);
 }
