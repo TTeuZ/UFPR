45c1facf0bc2a0c26c20cdfbbaa9c3dd42dc6cbd
TTeuZ <paulomateus.luza@hotmail.com>
2022-02-20 22:47:25 -0300

    [add] filtro de negativo

--- Programação 2/projeto1/pgmnega.c
+++ Programação 2/projeto1/pgmnega.c
@@ -6,20 +6,24 @@
 int main (int argc, char *argv[]) {
     image_f *image;
     params_f *params;
+    int row, col;
 
     /* Inicializa as imagens e os parametros */
     image = initialize_image ();
     params = initialize_params (image);
-
     /* tratamento dos parametros enviados para o filtro */
     treat_params (image, params, "", 0, argv, argc);
-
     /* chama a função que lê o pgm */
     read_image (image, params, argv);
+    
+    /* aplica o efeito de negativo */
+    for (row = 0; row < image->height; row++)
+        for (col = 0; col < image->width; col++) {
+            image->data[(row * image->width) + col] = image->max_value - image->data[(row * image->width) + col];
+        } 
 
     /* chama a função que grava a nova pgm */
     send_image (image, params, argv);
-
     /* Libera todo o espaço alocado */
     clean_structs (image, params);
 
