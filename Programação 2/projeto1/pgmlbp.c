c2613c2f58eace77793ec08637b53753c67adc97
TTeuZ <paulomateus.luza@hotmail.com>
2022-02-27 23:29:49 -0300

    [add] filtro de lbp

--- Programação 2/projeto1/pgmlbp.c
+++ Programação 2/projeto1/pgmlbp.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "core_functions.h"
 #include "image_functions.h"
 
 int main (int argc, char *argv[]) {
-    image_f *image;
+    image_f *image, *intern_image;
+    int row, col, row_l, col_l;
+    int lbp, limiar, count, count_i;
+    int values_array[7];
     params_f *params;
 
     params = initialize_params ();
@@ -13,10 +17,34 @@ int main (int argc, char *argv[]) {
     /* chama a função que lê o pgm */
     image = read_image (params, argv);
 
+    /* cria a imagem interna que vai ser enviada */
+    intern_image = initialize_image (image->type, image->height, image->width, image->max_value, params);
+
+    for (row = 0; row < intern_image->height; row++)
+        for (col = 0; col < intern_image->width; col++) {
+            count = 0;
+            for (row_l = -1; row_l < 2; row_l++) 
+                for (col_l = -1; col_l < 2; col_l++) 
+                    if (((row + row_l) >= 0 && (col + col_l) >= 0 ) && ((row + row_l) < image->height && (col + col_l) < image->width)) {
+                        limiar = image->data[(row * image->width) + col];
+                        if (row_l != 0 || col_l != 0) {
+                            values_array[count] = image->data[((row + row_l) * image->width) + (col + col_l)] >= limiar ? 1 : 0;
+                            count++;
+                        }
+                    }
+            lbp = 0;
+            for (count_i = 0; count_i < count; count_i++) {
+                values_array[count_i] = values_array[count_i] * pow (2, count_i);
+                lbp += values_array[count_i];
+            }
+            intern_image->data[(row * intern_image->width) + col] = lbp;
+        } 
+
     /* chama a função que grava a nova pgm */
-    send_image (image, params, argv);
+    send_image (intern_image, params, argv);
     /* Libera todo o espaço alocado */
     clean_structs (image, params);
+    clean_structs (intern_image, NULL);
 
     exit (EXIT_SUCCESS);
 }
