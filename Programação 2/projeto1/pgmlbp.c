3cdaa5a4e68b1dc4decf32b49e4d3d47ac3cf729
TTeuZ <paulomateus.luza@hotmail.com>
2022-02-27 23:46:07 -0300

    [fix] ajustes de comentarios e identação do filtro lbp

--- Programação 2/projeto1/pgmlbp.c
+++ Programação 2/projeto1/pgmlbp.c
@@ -20,19 +20,24 @@ int main (int argc, char *argv[]) {
     /* cria a imagem interna que vai ser enviada */
     intern_image = initialize_image (image->type, image->height, image->width, image->max_value, params);
 
+    /* Aplicação do filtro */
     for (row = 0; row < intern_image->height; row++)
         for (col = 0; col < intern_image->width; col++) {
             count = 0;
+            /* gera a 'matriz interna' com os valores desejados */
             for (row_l = -1; row_l < 2; row_l++) 
                 for (col_l = -1; col_l < 2; col_l++) 
-                    if (((row + row_l) >= 0 && (col + col_l) >= 0 ) && ((row + row_l) < image->height && (col + col_l) < image->width)) {
+                    /* verifica se os valores dessa nova matriz vão 'extrapolar' as bordas da imagem */
+                    if (((row + row_l) >= 0 && (col + col_l) >= 0) && ((row + row_l) < image->height && (col + col_l) < image->width)) {
                         limiar = image->data[(row * image->width) + col];
+                        /* pula o valor do meio */
                         if (row_l != 0 || col_l != 0) {
                             values_array[count] = image->data[((row + row_l) * image->width) + (col + col_l)] >= limiar ? 1 : 0;
                             count++;
                         }
                     }
             lbp = 0;
+            /* executa a varredura dos valores computados e soma em lbp */
             for (count_i = 0; count_i < count; count_i++) {
                 values_array[count_i] = values_array[count_i] * pow (2, count_i);
                 lbp += values_array[count_i];
