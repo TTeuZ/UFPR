42445d899b7e6eefbb0526aec32d9f92035f6f22
TTeuZ <pmla.luza@gmail.com>
2022-03-04 14:09:13 -0300

    [fix] ajustes no pgmrotação

--- Programação 2/projeto1/pgmrotacao.c
+++ Programação 2/projeto1/pgmrotacao.c
@@ -10,10 +10,8 @@
 int main (int argc, char *argv[]) {
     image_f *image, *intern_image;
     params_f *params;
+    int temp_col, temp_row, min_col, min_row, row, col, row_a, col_a;
     int degree, cong_degree;
-    int temp_h, temp_w;
-    int min_col, min_row;
-    int row, col, row_a, col_a;
     double radian, cong_radian;
     double cos_r, sin_r, cos_g, sin_g; 
 
@@ -25,6 +23,10 @@ int main (int argc, char *argv[]) {
     if (params->ex_param == -1)
         degree = 90;
     else degree = atoi (argv[(int) params->ex_param]);
+    
+    /* Reduz o angulo indicado para a 1° vota do circulo trigonométrico */
+    while (degree > 360)
+        degree = degree - 360;
 
     /* calcula o algulo congruente da imagem (considerando o 90° da borda a imagem 180 - 90 - degree == 90 - degree) */
     cong_degree = 90 - degree; 
@@ -38,26 +40,25 @@ int main (int argc, char *argv[]) {
     sin_g = sin (cong_radian);
 
     /* busca as novas altura e largura da imagem */
-    temp_w = abs (round (abs (cos_g * image->height) + abs (cos_r * image->width)));
-    temp_h = abs (round (abs (sin_g * image->height) + abs (sin_r * image->width)));
+    temp_col = abs (round (abs (cos_g * image->height) + abs (cos_r * image->width)));
+    temp_row = abs (round (abs (sin_g * image->height) + abs (sin_r * image->width)));
 
-    intern_image = initialize_image (image->type, temp_h, temp_w, 255, params);
+    intern_image = initialize_image (image->type, temp_row, temp_col, 255, params);
 
     /* Busca os valores min de cada eixo da nova imagem */
     min_col = (image->width * cos_r);
     min_row = (image->width * sin_r);
+    temp_col = (image->width * cos_r) - (image->height * sin_r);
+    temp_row = (image->width * sin_r) + (image->height * cos_r);
 
-    temp_w = (image->width * cos_r) - (image->height * sin_r);
-    temp_h = (image->width * sin_r) + (image->height * cos_r);
+    if (temp_col < min_col) min_col = temp_col;
+    if (temp_row < min_row) min_row = temp_row;
 
-    if (temp_w < min_col) min_col = temp_w;
-    if (temp_h < min_row) min_row = temp_h;
+    temp_col = (0 * cos_r) - (image->height * sin_r);
+    temp_row = (0 * sin_r) + (image->height * cos_r);
 
-    temp_w = (0 * cos_r) - (image->height * sin_r);
-    temp_h = (0 * sin_r) + (image->height * cos_r);
-
-    if (temp_w < min_col) min_col = temp_w;
-    if (temp_h < min_row) min_row = temp_h;
+    if (temp_col < min_col) min_col = temp_col;
+    if (temp_row < min_row) min_row = temp_row;
 
     if (degree <= 90) min_row = 0;
     if (degree <= 360 && degree > 270) min_col = 0;
