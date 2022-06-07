4d54c575025ed31b79f124e546874659cfa7752b
TTeuZ <paulomateus.luza@hotmail.com>
2022-06-07 20:53:10 -0300

    [add] função de criação/free do array de bicicletas

--- Programação 2/2022-1/projeto 1/bicycles.c
+++ Programação 2/2022-1/projeto 1/bicycles.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "bicycles.h"
+#include "bicycle.h"
+#include "constants.h"
+
+bicycles_f *inicialize_bicycles () {
+    bicycles_f *bicycles;
+    if (! (bicycles = malloc (sizeof (bicycles_f)))) {
+        fprintf (stderr, RED "[ERROR] " NC "Falha na alocação de memoria\n\n");
+        exit (EXIT_FAILURE);
+    } else {
+        bicycles->qtd = 0;
+        bicycles->bicycles = NULL;
+        return bicycles;
+    }
+}
+
+void clean_bicycles (bicycles_f *bicycles) {
+    int count;
+    if (bicycles->qtd != 0) 
+        for (count = 0; count < bicycles->qtd; count++)
+            free (bicycles->bicycles[count]);
+    free (bicycles);
+}
--- Programação 2/2022-1/projeto 1/bicycles.h
+++ Programação 2/2022-1/projeto 1/bicycles.h
@@ -3,10 +3,21 @@
 #ifndef BICYCLES_F
 #define BICYCLES_F
 typedef struct bicycles {
-    bicycle_f *bicycles;
+    bicycle_f **bicycles;
+    int qtd;
 } bicycles_f;
 
-// função de iniciar a lista de bicicletas
+/* 
+Função que inicializa a estrutura de dados de armazenamento das bicicletas encontradas nos logs
+Realiza a alocação inicial de espaço da propia estrutura e retorna o ponteiro de acesso
+*/
+bicycles_f *inicialize_bicycles ();
+
+/*
+Função que limpa a memoria ocupada pelo estrutura de armazenamento das bicicletas
+*/
+void clean_bicycles (bicycles_f *bicycles);
+
 // função para adicionar uma bibicleta na lista
 // função para verificar se uma bicicleta ja esta na lista
 // função para atualizar/adicionar informação em uma bicicleta que está na lista
