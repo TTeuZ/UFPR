897ddc01e15d9af92da2274e9afd67a8d5a3ba6f
TTeuZ <paulomateus.luza@hotmail.com>
2022-06-07 23:37:50 -0300

    [add] conjunto de funções de reader

---
+++ Programação 2/2022-1/projeto 1/reader.h
@@ -0,0 +1,8 @@
+#ifndef READER
+#define READER
+
+// Função que ele o diretorio e armazena o array de arquivos
+// Funçãoq ue le o arquivo e envia as informações pro lugar necessario
+// função que limpa o espaço dos arquivos lidos
+
+#endif
