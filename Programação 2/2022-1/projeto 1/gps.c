b69efccaf832f1e93c1e80ca19d9126a25d1f51b
TTeuZ <paulomateus.luza@hotmail.com>
2022-06-07 23:18:12 -0300

    [fix] função parser agora funciona caso não envie nenhum parametro

--- Programação 2/2022-1/projeto 1/gps.c
+++ Programação 2/2022-1/projeto 1/gps.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "constants.h"
 #include "bicycles.h"
 #include "bicycle.h"
 #include "parser.h"
@@ -13,6 +14,10 @@ int main (int argc, char *argv[]) {
 
     /* inicializa a estrutura de dados que irá armazenar as bicicletas */
     bicycles = inicialize_bicycles ();
+
+    /* Iniciando o processo de leitura dos logs */
+    fprintf (stdout, YELLOW "[PROCESSANDO] " NC "Iniciando leitura dos logs...\n\n");
+    fprintf (stdout, GREEN "[PROCESSADO] " NC "Leitura finalizada com sucesso\n\n");
 
     /* limpa os espaços alocados durante a execução do programa */
     clean_bicycles (bicycles);
--- Programação 2/2022-1/projeto 1/parser.c
+++ Programação 2/2022-1/projeto 1/parser.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <getopt.h>
 #include <unistd.h>
 #include "constants.h"
 #include "parser.h"
 
 char *parse_command_line (char *argv[], int argc, char *paramns) {
-    char *dirName;
+    char *dirName = "";
     int parameter;
     while ((parameter = getopt (argc, argv, paramns)) != -1) {
         switch (parameter) {
@@ -16,16 +17,21 @@ char *parse_command_line (char *argv[], int argc, char *paramns) {
             }
             case '?': {
                 if (optopt == 'd' && optarg == NULL) {
-                    fprintf (stderr, RED "[ERROR] " NC "é necessário indicar um diretorio de logs!\n");
-                    fprintf (stderr, RED "[ERROR] " NC "encerrando...\n\n");
+                    fprintf (stderr, RED "[ERROR] " NC "É necessário indicar um diretorio de logs!\n\n");
+                    fprintf (stderr, RED "[ERROR] " NC "Encerrando...\n\n");
                 } else {
-                    fprintf (stderr, RED "[ERROR] " NC "parametro invalido!\n");
-                    fprintf (stderr, RED "[ERROR] " NC "encerrando...\n\n");
+                    fprintf (stderr, RED "[ERROR] " NC "Parametro invalido!\n\n");
+                    fprintf (stderr, RED "[ERROR] " NC "Encerrando...\n\n");
                 }
                 exit (EXIT_FAILURE);
                 break;
             }
         }
+    }
+    if (strcmp(dirName, "") == 0) {
+        fprintf (stderr, RED "[ERROR] " NC "É necessário indicar um diretorio de logs!\n\n");
+        fprintf (stderr, RED "[ERROR] " NC "Encerrando...\n\n");
+        exit (EXIT_FAILURE);
     }
     return dirName;
 }
