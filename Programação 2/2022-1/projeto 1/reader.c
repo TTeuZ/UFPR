f37125476e4d16c945af4486cd88dfee073ebeb6
TTeuZ <paulomateus.luza@hotmail.com>
2022-06-08 20:55:37 -0300

    [add] função de leitura de diretorio e separação dos arquivos

--- Programação 2/2022-1/projeto 1/reader.c
+++ Programação 2/2022-1/projeto 1/reader.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <dirent.h>
 #include "constants.h"
 #include "reader.h"
 /*---------------------------------------------- Funções internas ----------------------------------------------*/
+/*
+* Função que retorna se o arquivo indicado possui '.log' em seu nome,
+* ou seja, é um arquivo de log
+*/
+int is_log_file (const char *file_name) {
+    size_t len = strlen (file_name);
+    return len > 4 && strcmp (file_name + len - 4, ".log") == 0;
+}
+
+/*
+* Função de filtro dos arquivos no diretorio, para deixar passar na função apenas os arquivos .log
+*/
 int filterFiles (const struct dirent *current_dir) {
-    printf("teste: %d\n", current_dir->d_type);
-    return 1;
+    if (current_dir->d_type == D_FILE && is_log_file (current_dir->d_name)) 
+        return 1;
+    else return 0;
 }
 /*---------------------------------------------- Funções internas ----------------------------------------------*/
 
@@ -25,12 +39,9 @@ directory_f *inicialize_directory () {
 
 directory_f *get_logs (char *dir_name) {
     directory_f *directory;
+
     directory = inicialize_directory ();
-
-
     directory->files_qtd = scandir (dir_name, &directory->files, filterFiles, alphasort);
-    // for (int teste = 0; teste < directory->files_qtd; teste++) {
-    //     printf("TESTE\n");
-    // }
+
     return directory;
 }
