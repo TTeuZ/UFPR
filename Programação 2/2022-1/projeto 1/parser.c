2f81ddce055d37a105dbd4e9cb13a23d9a5f9def
TTeuZ <paulomateus.luza@hotmail.com>
2022-06-08 16:49:10 -0300

    [fix] ajustes de nome de variavel no parser

--- Programação 2/2022-1/projeto 1/parser.c
+++ Programação 2/2022-1/projeto 1/parser.c
@@ -7,12 +7,17 @@
 #include "parser.h"
 
 char *parse_command_line (char *argv[], int argc, char *paramns) {
-    char *dirName = "";
+    char *dir_name = "";
     int parameter;
+    if (strcmp(dir_name, "") == 0) {
+        fprintf (stderr, RED "[ERROR] " NC "É necessário indicar um diretorio de logs!\n\n");
+        fprintf (stderr, RED "[ERROR] " NC "Encerrando...\n\n");
+        exit (EXIT_FAILURE);
+    }
     while ((parameter = getopt (argc, argv, paramns)) != -1) {
         switch (parameter) {
             case 'd': {
-                dirName = optarg;
+                dir_name = optarg;
                 break;
             }
             case '?': {
@@ -28,10 +33,5 @@ char *parse_command_line (char *argv[], int argc, char *paramns) {
             }
         }
     }
-    if (strcmp(dirName, "") == 0) {
-        fprintf (stderr, RED "[ERROR] " NC "É necessário indicar um diretorio de logs!\n\n");
-        fprintf (stderr, RED "[ERROR] " NC "Encerrando...\n\n");
-        exit (EXIT_FAILURE);
-    }
-    return dirName;
+    return dir_name;
 }
