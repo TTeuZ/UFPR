4336117449a100bcc5355308b4338750fc779658
TTeuZ <paulomateus.luza@hotmail.com>
2022-06-13 20:09:39 -0300

    [add] funções para adicionar bicicletas no conjunto

--- Programação 2/2022-1/projeto 1/bicycles.c
+++ Programação 2/2022-1/projeto 1/bicycles.c
@@ -18,24 +18,61 @@ bicycles_f *inicialize_bicycles () {
     }
 }
 
-void add_bicycle_log (bicycles_f *bicycles, bicycle_log_f *log) {
-    printf ("Nome: %s\n", log->bicycle_name);
-    printf ("data: %s\n", log->date);
-    printf ("Distancia: %f\n", log->distance);
-    printf ("velocidade media: %f\n", log->average_speed);
-    printf ("Velocidade maxima: %f\n", log->max_speed);
-    printf ("hr media: %d\n", log->average_hr);
-    printf ("hr maxima: %d\n", log->max_hr);
-    printf ("cadencia media: %d\n", log->average_cadence);
-    printf ("ganho de altimetria: %f\n", log->altimetry_gain);
-    printf ("\n");
+int already_added (bicycles_f *bicycles, char *bicycle_name) {
+    int count;
+    if (bicycles->qtd == 0) return -1;
+    for (count = 0; count < bicycles->qtd; count++) 
+        if (strcmp (bicycle_name, bicycles->bicycles[count]->name) == 0)
+            return count;
+    return -1;
 }
 
+void verify_and_add_bicycle (bicycles_f *bicycles, bicycle_log_f *log) {
+    bicycle_f *bicycle;
+    int pos;
+    pos = already_added (bicycles, log->bicycle_name);
+    if (pos != -1) {
+        add_bicycle_log (bicycles->bicycles[pos], log);
+    } else {
+        bicycle = create_bicycle (log->bicycle_name);
+        add_bicycle_log (bicycle, log);
+        add_bicycle_to_array (bicycles,  bicycle);
+    }
+}
+
+void add_bicycle_to_array (bicycles_f *bicycles, bicycle_f *bicycle) {
+    int count;
+    if (bicycles->qtd == 0) {
+        if (! (bicycles->bicycles = malloc (sizeof (bicycle_f)))) {
+            fprintf (stderr, RED "[ERROR] " NC "Falha na alocação de memoria\n\n");
+            fprintf (stderr, RED "[ERROR] " NC "Encerrando...\n\n");
+            exit (EXIT_FAILURE);
+        } else {
+            bicycles->bicycles[bicycles->qtd] = bicycle;
+            bicycles->qtd = 1;
+        }
+    } else {
+        if (! (bicycles->bicycles = realloc (bicycles->bicycles, (sizeof (bicycle_f) * (bicycles->qtd * 1))))) {
+            fprintf (stderr, RED "[ERROR] " NC "Falha na alocação de memoria\n\n");
+            fprintf (stderr, RED "[ERROR] " NC "Encerrando...\n\n");
+            exit (EXIT_FAILURE);
+        } else {
+            count = bicycles->qtd;
+            while (count != 0 && strcmp (bicycle->name, bicycles->bicycles[count-1]->name) <= 0) {
+                bicycles->bicycles[count] = bicycles->bicycles[count - 1];
+                count--;
+            }
+            bicycles->bicycles[count] = bicycle;
+            bicycles->qtd++;
+        }
+    }
+}
 
 void clean_bicycles (bicycles_f *bicycles) {
     int count;
     if (bicycles->qtd != 0) 
         for (count = 0; count < bicycles->qtd; count++)
             clean_bicycle (bicycles->bicycles[count]);
+    free (bicycles->bicycles);
     free (bicycles);
 }
--- Programação 2/2022-1/projeto 1/bicycles.h
+++ Programação 2/2022-1/projeto 1/bicycles.h
@@ -14,11 +14,23 @@ typedef struct bicycles {
 bicycles_f *inicialize_bicycles ();
 
 /*
+* Funçãoq ue verifica se a bicicleta ja esta no conjunto
+* Retorna a posição da bicicleta no conjunto caso ja esteja adicionada 
+* Retorna -1 caso contrario
+*/
+int already_added (bicycles_f *bicycles, char *bicycle_name);
+
+/*
 * Função que adiciona o log a uma bicicleta, ou cria uma nova bicicleta caso ainda não exista
 * Verifica se ja existe uma bicicleta para o log enviado, caso exista adiciona o log nela
 * caso não exista, cria uma nova bicicleta a adiciona o conjunto de bicicletas
 */
-void add_bicycle_log (bicycles_f *bicycles, bicycle_log_f *log);
+void verify_and_add_bicycle (bicycles_f *bicycles, bicycle_log_f *log);
+
+/*
+* Função que adiciona uma nova bicicleta ao conjunto de bicicletas
+*/
+void add_bicycle_to_array (bicycles_f *bicycles, bicycle_f *bicycle);
 
 /*
 * Função que limpa a memoria ocupada pelo estrutura de armazenamento das bicicletas
@@ -27,7 +39,5 @@ void add_bicycle_log (bicycles_f *bicycles, bicycle_log_f *log);
 */
 void clean_bicycles (bicycles_f *bicycles);
 
-// função para verificar se uma bicicleta ja esta na lista
-// função para atualizar/adicionar informação em uma bicicleta que está na lista
 // função que gera um retorno da lista de atividades das bicicletas de acordo com um sort especificado
 #endif
