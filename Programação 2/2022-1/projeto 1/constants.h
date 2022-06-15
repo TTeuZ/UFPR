15808331011520d41d7cd3c4940e768e9ceaf564
TTeuZ <paulomateus.luza@hotmail.com>
2022-06-14 21:54:54 -0300

    [add] constants de print

--- Programação 2/2022-1/projeto 1/constants.h
+++ Programação 2/2022-1/projeto 1/constants.h
@@ -1,11 +1,17 @@
 #ifndef CONSTANTS
 #define CONSTANTS
 
+/* colaração dos prints */
 #define RED "\e[0;31m"
 #define GREEN "\e[0;32m"
 #define YELLOW "\e[1;33m"
 #define NC "\e[0m"
 
-#define INPUT_SIZE 1024
+/* Prints de resumo */
+#define DATE_SORT 1
+#define DISTANCE_SORT 2
+#define ALTIMETRY_SORT 3
+#define GROUPED 1
+#define NOT_GROUPED 0
 
 #endif
