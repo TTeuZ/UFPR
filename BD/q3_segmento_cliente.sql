31c136aa4cd47978c5cae2ed96c92be3a7c4a587
TTeuZ <paulomateus.luza@hotmail.com>
2022-06-25 23:21:08 -0300

    [add] wip trabalho 1 de BD

---
+++ BD/q3_segmento_cliente.sql
@@ -0,0 +1,1 @@
+select distinct(C_MKTSEGMENT) , count(C_MKTSEGMENT) from CUSTOMER group by C_MKTSEGMENT;
---
+++ BD/q5_qtd_encomendas.sql
@@ -0,0 +1,1 @@
+select N_NAME, count(O_ORDERKEY) from ORDERS inner join CUSTOMER on ORDERS.O_CUSTKEY = CUSTOMER.C_CUSTKEY inner join NATION on CUSTOMER.C_NATIONKEY = NATION.N_NATIONKEY group by N_NAME order by count(O_ORDERKEY) desc;
