61bfb9abf5fb617787159a29e467c8065f2088fa
TTeuZ <paulomateus.luza@hotmail.com>
2022-08-30 23:46:01 -0300

    [wip] equivalencia por visão

--- BD/trabalho 2/equivalentView.c
+++ BD/trabalho 2/equivalentView.c
@@ -1,5 +1,18 @@
 #include "equivalentView.h"
 
+//------------------------------------- Funções interanas -------------------------------------
+/*!
+    \brief Função que pega a proxima permutação do array de ids
+    \param ids Vetor de ids da transação
+    \param left Posição mais a esquerda da permutação atual
+    \param right Posição mais a direita da permutação atual
+*/
+void next_permutation (int *ids, int *left, int *right) {
+
+}
+
+//------------------------------------- Funções interanas -------------------------------------
+
 int generate_vision (schedule_t *schedule, schedule_t **vision, int *ids) {
     operation_t *aux, *operation;
     schedule_t *temp;
@@ -26,37 +39,39 @@ int generate_vision (schedule_t *schedule, schedule_t **vision, int *ids) {
 }
 
 int check_equivalence (schedule_t *schedule) {
-    // schedule_t *vision = NULL;
-    // operation_t *aux;
-    // int possible_sequences;
-    // int count;
-    // int *ids;
+    schedule_t *vision = NULL;
+    operation_t *aux;
+    int possible_sequences, count, left, right;
+    int *ids;
 
-    // if (! (ids = calloc (schedule->transactions_qtd + 1, sizeof (int))))
-    //     return EXIT_FAILURE;
+    if (! (ids = calloc (schedule->transactions_qtd, sizeof (int))))
+        return EXIT_FAILURE;
     
-    // memset (ids, 0, sizeof (int) * (schedule->transactions_qtd + 1));
-    // for (count = 0; count < schedule->transactions_qtd; count++)
-    //     ids[count] = count + 1;
+    memset (ids, 0, sizeof (int) * (schedule->transactions_qtd + 1));
+    for (count = 0; count < schedule->transactions_qtd; count++)
+        ids[count] = schedule->transactions_ids[count];
 
-    // possible_sequences = fat (schedule->transactions_qtd);
+    left = 0;
+    right = schedule->transactions_qtd - 1;
+    possible_sequences = fat (schedule->transactions_qtd);
 
-    // for (count = 0; count < possible_sequences; count++) {
-    //     generate_vision (schedule, &vision, ids);
+    for (count = 0; count < possible_sequences; count++) {
+        generate_vision (schedule, &vision, ids);
 
-    //     aux = vision->start;
-    //     while (aux != NULL) {
-    //         printf ("%d %d %c %c\n", aux->timestamp, aux->transaction_id, aux->op_type, aux->attribute);
-    //         aux = aux->next;
-    //     }
-    //     printf ("Tamanho: %d - qtd de transaçoes %d\n", vision->size, vision->transactions_qtd);
+        aux = vision->start;
+        while (aux != NULL) {
+            printf ("%d %d %c %c\n", aux->timestamp, aux->transaction_id, aux->op_type, aux->attribute);
+            aux = aux->next;
+        }
+        printf ("Tamanho: %d - qtd de transaçoes %d\n", vision->size, vision->transactions_qtd);
 
 
 
-    //     destroy_schedule (vision);
-    // }
+        destroy_schedule (vision);
+        next_permutation (ids, &left, &right);
+    }
     
 
-    // free (ids);
+    free (ids);
     return EXIT_SUCCESS;
 }
