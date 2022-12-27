f177e9ee52631b16cdd04e935eb6973a6958f872
TTeuZ <paulomateus.luza@hotmail.com>
2022-12-27 15:02:25 -0300

    [wip] print do mapa do jogo

--- Paradigmas/trabalho/src/functions/Printer.java
+++ Paradigmas/trabalho/src/functions/Printer.java
@@ -46,29 +46,30 @@ public class Printer implements Constants {
 
     // ---------------------------- Public Methods ----------------------------
     public void print(Sector[][] board, SimplePlayer playerOne, SupportPlayer playerTwo) {
-        int x, y, count;
-        Player tempArray[];
+        int playerCount, attackCount, defenseCount;
         boolean sameSector;
+        Player tempArray[];
+        Sector tempSector;
+        int x, y, count;
 
         tempArray = new Player[2];
         tempArray[0] = playerOne;
         tempArray[1] = playerTwo;
-        count = 0;
+
+        playerCount = 1;
+        attackCount = defenseCount = 0;
 
         if (playerOne.getX() == playerTwo.getX() && playerOne.getY() == playerTwo.getY())
             sameSector = true;
         else
             sameSector = false;
 
-
-        // verificar se o P1 e o P2 estão no mesmo setor
-        // armazenar os inimigos do setor, ou dos setores para o print
-
         System.out.println("----------------------------");
         System.out.println("|   Antivìrus por um dia   |");
         System.out.println("----------------------------");
         System.out.println("      1   2   3   4   5");
         for (x = 0; x < BOARD_ROWS; ++x) {
+            // Print da mesa do jogo
             for (y = 0; y < BOARD_COLS; ++y) {
                 if (BOARD_BASE[x][y] == 't')
                     System.out.printf("%c", verifyWall(board, x, y - 5, TOP));
@@ -80,30 +81,47 @@ public class Printer implements Constants {
                     System.out.printf("%c", BOARD_BASE[x][y]);
             }
 
+            // Espaçamento
+            System.out.printf("     ");
+
+            // Print do menu de cada jogador
             if (sameSector) {
-                for (y = 0; y < MENU_SAME_SECTOR_COLS; ++y) {
+                tempSector = board[playerOne.getX()][playerOne.getY()];
+                for (y = 0; y < MENU_COLS; ++y) {
                     if (MENU_SAME_SECTOR_BASE[x][y] == 'x')
                         System.out.printf("%d", playerOne.getX());
                     else if (MENU_SAME_SECTOR_BASE[x][y] == 'y')
                         System.out.printf("%d", playerOne.getY());
-                    else if (MENU_SAME_SECTOR_BASE[x][y] == 'p') {
-                        System.out.printf("P%d", count + 1);
-                        if (count == 1) count = 0;
-                        count++;
-                    } else if (MENU_SAME_SECTOR_BASE[x][y] == 'a') {
-                        System.out.printf("%d", tempArray[count].getAttack());
-                    } else if (MENU_SAME_SECTOR_BASE[x][y] == 'd') {
-                        System.out.printf("%d", tempArray[count].getDefense());
-                        if (count == 1) count = 0;
-                        count++;
-                    }
-                    else 
+                    else if (MENU_SAME_SECTOR_BASE[x][y] == 'p')
+                        System.out.printf("P%d", playerCount++);
+                    else if (MENU_SAME_SECTOR_BASE[x][y] == 'a')
+                        System.out.printf("%d", tempArray[attackCount++].getAttack());
+                    else if (MENU_SAME_SECTOR_BASE[x][y] == 'd')
+                        System.out.printf("%d", tempArray[defenseCount++].getDefense());
+                    else
                         System.out.printf("%c", MENU_SAME_SECTOR_BASE[x][y]);
                 }
             } else {
-
+                for (count = 0; count < tempArray.length; ++count) {
+                    for (y = 0; y < MENU_COLS; ++y) {
+                        tempSector = board[tempArray[count].getX()][tempArray[count].getY()];
+                        if (MENU_DIFF_SECTOR_BASE[x][y] == 'x')
+                            System.out.printf("%d", tempArray[count].getX());
+                        else if (MENU_DIFF_SECTOR_BASE[x][y] == 'y')
+                            System.out.printf("%d", tempArray[count].getY());
+                        else if (MENU_DIFF_SECTOR_BASE[x][y] == 'p')
+                            System.out.printf("P1");
+                        else if (MENU_DIFF_SECTOR_BASE[x][y] == 'a')
+                            System.out.printf("%d", tempArray[count].getAttack());
+                        else if (MENU_DIFF_SECTOR_BASE[x][y] == 'd')
+                            System.out.printf("%d", tempArray[count].getDefense());
+                        else
+                            System.out.printf("%c", MENU_DIFF_SECTOR_BASE[x][y]);
+                    }
+                    // Espaçamento
+                    System.out.printf("   ");
+                }
             }
-
             System.out.printf("\n");
         }
     }
