720c71fdc06e27df7422a9e514a8fa9a959d8e8e
TTeuZ <paulomateus.luza@hotmail.com>
2023-01-04 16:46:35 -0300

    [fix] tirando um if de um trabalho antigo

--- BD/trabalho 2/escalona.c
+++ BD/trabalho 2/escalona.c
@@ -6,45 +6,45 @@
 #include "serializable.h"
 #include "equivalentView.h"
 
-int main () {
-    schedule_t *schedule = NULL;
-    escalations_t *escalations = NULL;
+int main() {
+    schedule_t* schedule = NULL;
+    escalations_t* escalations = NULL;
     int is_serializable, is_equivalent;
     int count, i_count;
 
-    schedule_t *aux;
+    schedule_t* aux;
     int qtd;
 
-    if (! (escalations = alloc_escalations ()))
+    if (!(escalations = alloc_escalations()))
         return EXIT_FAILURE;
 
-    if (read_input_schedule (&schedule))
+    if (read_input_schedule(&schedule))
         return EXIT_FAILURE;
 
-    if (separete_schedule (schedule, escalations))
+    if (separete_schedule(schedule, escalations))
         return EXIT_FAILURE;
 
     for (count = 0; count < escalations->qtd; count++) {
         aux = escalations->list[count];
         qtd = aux->transactions_qtd;
 
-        is_serializable = check_serializable (aux);
-        is_equivalent = check_equivalence (aux);
+        is_serializable = check_serializable(aux);
+        is_equivalent = check_equivalence(aux);
 
-        printf ("%d ", count + 1);
+        printf("%d ", count + 1);
         for (i_count = 0; i_count < qtd; i_count++)
-            printf ("%d%c", aux->transactions_ids[i_count], (i_count + 1) == qtd ? ' ' : ',');
+            printf("%d%c", aux->transactions_ids[i_count], (i_count + 1) == qtd ? ' ' : ',');
 
-        if (is_serializable) printf ("SS ");
-        else printf ("NS ");
+        if (is_serializable) printf("SS ");
+        else printf("NS ");
 
-        if (is_equivalent) printf ("SV");
-        else printf ("NV");
+        if (is_equivalent) printf("SV");
+        else printf("NV");
 
-        if (count + 1 != escalations->qtd) printf ("\n");
+        printf("\n");
     }
 
-    destroy_escalations (escalations);
-    free (schedule);
+    destroy_escalations(escalations);
+    free(schedule);
     return EXIT_SUCCESS;
 }
