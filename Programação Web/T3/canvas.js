25a41182502c0ea0901d8caa04e4b7704571b2b3
TTeuZ <paulomateus.luza@hotmail.com>
2023-04-24 23:39:02 -0300

    [wip] T3 de prog web

--- Programação Web/T3/canvas.js
+++ Programação Web/T3/canvas.js
@@ -2,21 +2,18 @@
 var canvas = document.getElementById("canvas")
 var context = canvas.getContext("2d")
 
-const point = (x, y) => ({x, y})
-const line = (begin, end) => ({begin, end})
+const point = (x, y) => ({ x, y })
+const line = (begin, end) => ({ begin, end })
 const mouse = {
     x: 0, y: 0,
     button: 0,
     dragging: false,
     dragX: 0, dragY: 0
 }
-
 var lines = []
-const size = 600
-const section = size / 6
 
-// input functions
-function validateInput (value) {
+// General Functions
+function validateInput(value) {
     if (value > 8) value = 8
     if (value < 3) value = 3
 
@@ -24,78 +21,58 @@ function validateInput (value) {
     input.value = value
 }
 
-function createPoligon () {
-    clearLines()
-
+function treatInput() {
     const input = document.getElementById("sides")
-    const value = input.value
-
-    switch (value) {
-        case '3':
-            lines = createLines(3, 60, 50)
-        break
-        case '4':
-        break
-        case '5':
-        break
-        case '6':
-        break
-        case '7':
-        break
-        case '8':
-        break
-    }
+    createPolygon(parseInt(input.value))
+    draw()
 }
 
-function createLines(sides, ang, lineSize) {
-    var beginX, beginY, endX, endY
-    var radius, cos, sin
-    var count, sinal
+function cleanCanvas() { context.clearRect(0, 0, 600, 600) }
 
-    sinal = 1
-    beginX = section * 2
-    beginY = section * 3
-    for (count = 0; count < sides; ++count) {
-        radius = ((sinal * ang) * Math.PI) / 180
-        cos = Math.cos(radius)
-        sin = Math.sin(radius)
-    }
-}
+function clearLines() { lines = [] }
 
-// canva function
-function cleanCanvas () { context.clearRect(0,0,size,size) }
-
-// Line functions
-function clearLines () { lines = [] }
-
-function drawLine (line) {
+// Draw Functions
+function drawLine(line) {
     context.moveTo(line.begin.x, line.begin.y)
     context.lineTo(line.end.x, line.end.y)
 }
 
-function drawLines () {
+function drawLines() {
     let count
     for (count = 0; count < lines.length; ++count)
         drawLine(lines[count])
 }
 
-// Main functions
-function normalBegin () {
-    clearLines()
-    
-    const begin = point(section * 2, section * 3)
-    const end = point(section * 4, section * 3)
-    const newLine = line(begin, end)
-    lines.push(newLine)
-}
-
-function draw () {
+function draw() {
     context.beginPath()
     cleanCanvas()
     drawLines()
     context.stroke()
 }
 
+// Polygon functions
+function createPolygon(sides) {
+    var sideSize, center, x, y, begin, end, newLine, count
+
+    clearLines()
+
+    sideSize = 150
+    center = 300
+
+    x = center + sideSize * Math.cos(0)
+    y = center + sideSize * Math.sin(0)
+    begin = point(x, y)
+
+    for (count = 1; count <= sides; ++count) {
+        x = center + sideSize * Math.cos(count * 2 * Math.PI / sides)
+        y = center + sideSize * Math.sin(count * 2 * Math.PI / sides)
+        end = point(x, y)
+        newLine = line(begin, end)
+        lines.push(newLine)
+        begin = end
+    }
+}
+
 // main
-normalBegin()
+createPolygon(2)
 draw()
