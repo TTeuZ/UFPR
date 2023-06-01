68471d8e141fb1be3fd78417ce0f48c2fb4e576f
TTeuZ <paulomateus.luza@hotmail.com>
2023-06-01 18:35:15 -0300

    [add] home page

--- Programação Web/T5/config/routes.rb
+++ Programação Web/T5/config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   resources :addresses
   resources :students
   resources :colleges
+  resources :home
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
 end
