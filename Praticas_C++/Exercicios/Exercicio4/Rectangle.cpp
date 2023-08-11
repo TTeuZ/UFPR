0d5e620d9a6db8526865dab88adc490a2ee54cfa
TTeuZ <pmla.luza@gmail.com>
2023-08-11 14:47:25 -0300

    [add] base para o exercicio 4

---
+++ Praticas_C++/Exercicios/Exercicio4/Rectangle.cpp
@@ -0,0 +1,33 @@
+#include "Rectangle.hpp"
+
+#include <iostream>
+#include <string>
+
+// Getters
+std::string Rectangle::getColor() { return color; };
+
+uint32_t Rectangle::getWidth() { return width; };
+
+uint32_t Rectangle::getHeight() { return height; };
+
+// Setters
+void Rectangle::setColor(std::string newColor) { color = newColor; };
+
+void Rectangle::setWidth(uint32_t newWidth) {
+    width = newWidth > 0 ? newWidth : 0;
+};
+
+void Rectangle::setHeight(uint32_t newHeight) {
+    height = newHeight > 0 ? newHeight : 0;
+};
+
+// Functions
+void Rectangle::introduce() {
+    std::cout << "\tCor de preenchimento: " << color << std::endl;
+    std::cout << "\tLargura: " << width << std::endl;
+    std::cout << "\tAltura: " << height << std::endl;
+}
+
+uint32_t Rectangle::perimeter() { return (height * 2) + (width * 2); };
+
+uint32_t Rectangle::area() { return height * width; };
---
+++ Praticas_C++/Exercicios/Exercicio4/Rectangle.hpp
@@ -0,0 +1,30 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include <cstdint>
+#include <string>
+
+class Rectangle {
+   public:
+    // Getters
+    std::string getColor();
+    uint32_t getWidth();
+    uint32_t getHeight();
+
+    // Setters
+    void setColor(std::string newColor);
+    void setWidth(uint32_t newWidth);
+    void setHeight(uint32_t newHeight);
+
+    // Funtcions
+    void introduce();
+    uint32_t perimeter();
+    uint32_t area();
+
+   private:
+    std::string color;
+    uint32_t width;
+    uint32_t height;
+};
+
+#endif
