ddd32838bcc85f908157a49753028b14f106882c
TTeuZ <paulomateus.luza@hotmail.com>
2023-08-19 00:01:30 -0300

    [wip] lab 1 parte 2

--- Machine Learning/Lab1/representation.py
+++ Machine Learning/Lab1/representation.py
@@ -19,77 +19,97 @@ def getBinaryFile(file):
 
 def getChunkConfig(file):
     chunkConfig = []
-    for j in range(3):
-        for i in range(2):
-            wStart = i * (file['width'] // 2)
-            wEnd = (file['width'] // 2) if i == 0 else file['width']
-            hStart = j * (file['height'] // 3)
-            hEnd = (j + 1) * (file['height'] // 3) if j < 2 else file['height']
+    for h in range(3):
+        for w in range(2):
+            wStart = w * (file['width'] // 2)
+            wEnd = (file['width'] // 2) if w == 0 else file['width']
+            hStart = h * (file['height'] // 3)
+            hEnd = (h + 1) * (file['height'] // 3) if h < 2 else file['height']
             chunkConfig.append((wStart, wEnd, hStart, hEnd))
     return chunkConfig
 
 
 # chunk[0] = wStart | chunk[1] = wEnd | chunk[2] = hStart | chunk[3] = hEnd
+# the position of x and y are inverted in everythin
+# pixel(w, h)
 def getResult(chunk, file, pixel):
     result = ''
 
     # pixel para cima
+    # hasFoundBlackLeft = False
+    # hasFoundBlackRight = False
     for i in range(chunk[2], pixel[1]):
-        # print(file['image'][i][pixel[0]])
-        print(i)
+        if file['image'][i][pixel[0]] == 1 and 'u' not in result:
+            result += 'u'
 
-    # pixel para cima
-    # for i in range(chunk[2], pixel[1]):
-    #     if file['image'][pixel[0]][i] == 1:
-    #         result += 'u'
+        # if 'u' not in result:
+        #     blackInThisRow = False
+        #     for j in range(chunk[0], pixel[0]):
+        #         if file['image'][i][j] == 1:
+        #             hasFoundBlackLeft = blackInThisRow = True
+        #         if j == (pixel[0] - 1) and hasFoundBlackLeft and not blackInThisRow:
+        #             return '0'
 
-    #     # for j in range(chunk[0], pixel[0]):
-    #     #     if file['image'][j][i] == 1:
-    #     #         return '0'
+        #     blackInThisRow = False
+        #     for j in range(pixel[0], chunk[1]):
+        #         if file['image'][i][j] == 1:
+        #             hasFoundBlackLeft = blackInThisRow = True
+        #         if j == (chunk[1] - 1) and hasFoundBlackRight and not blackInThisRow:
+        #             return '1'
 
-    #     # for j in range(pixel[0], chunk[1]):
-    #     #     if file['image'][j][i] == 1:
-    #     #         return '1'
+    # pixel para direita
+    for i in range(pixel[0], chunk[1]):
+        if file['image'][pixel[1]][i] == 1 and 'r' not in result:
+            result += 'r'
 
-    # # pixel para direita
-    # for i in range(pixel[0], chunk[1]):
-    #     if file['image'][i][pixel[1]] == 1:
-    #         result += 'r'
+    # pixel para baixo
+    # hasFoundBlackLeft = False
+    # hasFoundBlackRight = False
+    for i in range(pixel[1], chunk[3]):
+        if file['image'][i][pixel[0]] == 1 and 'd' not in result:
+            result += 'd'
 
-    # # # pixel para baixo
-    # for i in range(pixel[1], chunk[3]):
-    #     if file['image'][pixel[0]][i] == 1:
-    #         result += 'd'
+        # if 'd' not in result:
+        #     blackInThisRow = False
+        #     for j in range(chunk[0], pixel[0]):
+        #         if file['image'][i][j] == 1:
+        #             hasFoundBlackLeft = blackInThisRow = True
+        #         if j == (pixel[0] - 1) and hasFoundBlackLeft and not blackInThisRow:
+        #             return '2'
 
-    #     # for j in range(chunk[0], pixel[0]):
-    #     #     if file['image'][j][i] == 1:
-    #     #         return '2'
+        #     blackInThisRow = False
+        #     for j in range(pixel[0], chunk[1]):
+        #         if file['image'][i][j] == 1:
+        #             hasFoundBlackLeft = blackInThisRow = True
+        #         if j == (chunk[1] - 1) and hasFoundBlackRight and not blackInThisRow:
+        #             return '3'
 
-    #     # for j in range(pixel[0], chunk[1]):
-    #     #     if file['image'][j][i] == 1:
-    #     #         return '3'
+    # pixel para esquerda
+    for i in range(chunk[0], pixel[0]):
+        if file['image'][pixel[1]][i] == 1 and 'l' not in result:
+            result += 'l'
 
-    # # # pixel para esquerda
-    # for i in range(chunk[0], pixel[0]):
-    #     if file['image'][i][pixel[1]] == 1:
-    #         result += 'l'
-
-    return str(result)
+    return result
 
 
 def getConcavity(file, chunkConfig):
     concavity = []
     for chunk in chunkConfig:
+        # chunkArea = (chunk[1] - chunk[0]) * (chunk[3] - chunk[2])
+
+        # chunkConcavity = {'ur': 0, 'rd': 0, 'dl': 0, 'ul': 0, 'urd': 0, 'rdl': 0, 'udl': 0, 'url': 0, 'urdl': 0, '0': 0, '1': 0, '2': 0, '3': 0}
         chunkConcavity = {'ur': 0, 'rd': 0, 'dl': 0, 'ul': 0, 'urd': 0, 'rdl': 0,
-                          'udl': 0, 'url': 0, 'urdl': 0, '0': 0, '1': 0, '2': 0, '3': 0}
-        for i in range(chunk[2], chunk[3]):
-            for j in range(chunk[0], chunk[1]):
-                if file['image'][i][j] != 1:
-                    result = getResult(chunk, file, (i, j))
-                    print(result)
-                    # if result in chunkConcavity.keys():
-                    # chunkConfig[result] += 1
+                          'udl': 0, 'url': 0, 'urdl': 0, 'u': 0, 'r': 0, 'd': 0, 'l': 0}
+        for h in range(chunk[2], chunk[3]):
+            for w in range(chunk[0], chunk[1]):
+                if file['image'][h][w] != 1:
+                    result = getResult(chunk, file, (w, h))
+                    if result in chunkConcavity.keys():
+                        chunkConcavity[result] += 1
+
         concavity += chunkConcavity.values()
+        # concavity += [value//(chunkArea if chunkArea != 0 else 1)
+        #               for value in chunkConcavity.values()]
 
     return concavity
 
@@ -97,13 +117,14 @@ def getConcavity(file, chunkConfig):
 def getBlackPixelDensity(binarizedFile, chunkConfig):
     densityByChunck = []
     for chunk in chunkConfig:
-        chunkArea = (chunk[1] - chunk[0]) * (chunk[3] - chunk[2])
+        # chunkArea = (chunk[1] - chunk[0]) * (chunk[3] - chunk[2])
         pixels = 0
-        for i in range(chunk[2], chunk[3]):
-            for j in range(chunk[0], chunk[1]):
-                if binarizedFile['image'][i][j] == 1:
+        for h in range(chunk[2], chunk[3]):
+            for w in range(chunk[0], chunk[1]):
+                if binarizedFile['image'][h][w] == 1:
                     pixels += 1
-        # density = pixels / chunkArea
+
+        # density = pixels / chunkArea if chunkArea != 0 else pixels
         density = pixels
         densityByChunck.append(density)
 
@@ -112,7 +133,7 @@ def getBlackPixelDensity(binarizedFile, chunkConfig):
 
 if __name__ == '__main__':
     representation = []
-    files = getFilesList("digits/test.txt")
+    files = getFilesList("digits/files.txt")
 
     for file in files:
         imageRep = []
@@ -120,6 +141,9 @@ if __name__ == '__main__':
         # Preparing the data
         binarizedFile = getBinaryFile(file[0])
         chunkConfig = getChunkConfig(binarizedFile)
+
+        # print(binarizedFile['height'], binarizedFile['width'])
+        # print(chunkConfig)
 
         # Getting representation
         concavity = getConcavity(binarizedFile, chunkConfig)
@@ -130,6 +154,16 @@ if __name__ == '__main__':
         imageRep += [file[1]]
         representation.append(imageRep)
 
-    # for rep in representation:
-        # print(' '.join(str(r) for r in rep))
+    for rep in representation:
+        print(' '.join(str(r) for r in rep))
         # print(len(rep))
+
+    # for chunk in chunkConfig:
+    #     for h in range(chunk[2], chunk[3]):
+    #         for w in range(chunk[0], chunk[1]):
+    #             print(binarizedFile['image'][h][w])
+
+        # for h in range(chunkConfig[0][2], chunkConfig[0][3]):
+        #     for w in range(chunkConfig[0][0], chunkConfig[0][1]):
+        #         print(binarizedFile['image'][h][w], end='')
+        #     print('\n', end='')
