8e90f50380ff92e97bc703851aa66d9f50e849de
TTeuZ <pmla.luza@gmail.com>
2023-10-11 14:35:48 -0300

    [add] exemplos da aula 15

---
+++ Praticas_C++/Exemplos em aulas/15 -  Herança multipla/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "ProfessorEngenheiro.hpp"
+
+int main() {
+    ProfessorEngenheiro pe{"Maria", 11111111111, 85, 40, 1234};
+
+    // std::cout << pe.getNome() >> std::endl;
+
+    return 0;
+}
