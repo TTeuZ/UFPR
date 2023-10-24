867f86e951127363cd323b72b9ce3498faf7ae1e
TTeuZ <pmla.luza@gmail.com>
2023-10-23 22:34:30 -0300

    [add] display class

--- Praticas_C++/CrazyGuitar/Src/Functionals/Display/Display.cpp
+++ Praticas_C++/CrazyGuitar/Src/Functionals/Display/Display.cpp
@@ -1,1 +1,13 @@
 #include "Display.hpp"
+
+Display::Display(const uint16_t widht, const uint16_t height)
+    : widht{widht}, height{height} {
+    // TODO: Add exception treatment
+    this->display = al_create_display(this->widht, this->height);
+    this->buffer = al_create_bitmap(this->widht, this->height);
+}
+
+Display::~Display() {
+    al_destroy_display(this->display);
+    al_destroy_bitmap(this->buffer);
+}
