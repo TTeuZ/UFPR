d6f7259bede0d8003cd8d196dbf017f6cae81689
TTeuZ <paulomateus.luza@hotmail.com>
2023-10-29 17:18:51 -0300

    [wip] lab3

---
+++ Machine Learning/Lab3/word2vec/scripts/classification_tests.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import load_svmlight_file
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+from sklearn import svm
+
+import config
+
+# Reading the trainning and testing features
+x_train, y_train = load_svmlight_file(f"../features/train_{config.VectorSize}_{config.WindowSize}_{config.MinCount}.txt")
+x_test, y_test = load_svmlight_file(f"../features/test_{config.VectorSize}_{config.WindowSize}_{config.MinCount}.txt")
+
+# x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.33, random_state=42)
+
+model = svm.SVC()
+model.fit(x_train, y_train)
+
+pred = model.predict(x_test)
+
+cm = confusion_matrix(y_test, pred)
+for i in range(2):
+    for j in range(2):
+        print(cm[i][j], end=' ')
+    print('\n', end='')
+
+print("\n\n")
+print(classification_report(y_test, pred))
