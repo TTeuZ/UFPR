f62ae7344cc9faea5299d1970c47e749e5da7786
TTeuZ <pmla.luza@gmail.com>
2023-10-31 17:01:32 -0300

    [tip] assignment attempt

--- Praticas_C++/CrazyGuitar/Src/Game/Game.hpp
+++ Praticas_C++/CrazyGuitar/Src/Game/Game.hpp
@@ -1,6 +1,21 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
-class Game {};
+#include "../Functionals/Display/Display.hpp"
+
+class Game {
+   public:
+    /**
+     * Instanciate an Game object, that will contain all the information
+     * necessary to play the game
+     *
+     * @param width Game display width
+     * @param height Game DIsplay height
+     */
+    Game(const Display& display);
+
+   private:
+    Display& display;
+};
 
 #endif
